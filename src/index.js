7d577a11810010f92f9720b4a2b52b229c7dda40
terrellstewart <terrellstewart0@gmail.com>
2017-11-14 09:32:35 -0600

    Got text on screen

--- src/index.js
+++ src/index.js
@@ -22,24 +22,26 @@ getMovies().then((movies) => {
   console.log(error);
 });
 
-$.get('db.json').done(function (data) {
+ $.get('api/movies').done(function (data) {
     buildHTML(data);
 }).fail(function (jqXhr, status, error) {
     console.log("there was an error!");
     console.log("response status:" + status);
     console.log("check console log for" + error)
 });
-
+// text on screen
     function buildHTML(movies) {
         let moviesHTML = "";
         movies.forEach(function (movies) {
 
 
             moviesHTML += "<div class='movies'>";
-            moviesHTML += "<h6>" + movies.title
+            moviesHTML += "<tr>" + movies.title + " </tr>";
+            moviesHTML += "<td>" + movies.rating + " </td>"
 
 
+        });
+        $("#movies").html(moviesHTML);
+}
 
 
-        })
-    }
