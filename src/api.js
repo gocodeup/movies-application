9957a767ea696148e0d75dc502df7e66d216e4b8
terryhale <aggiefan17@yahoo.com>
2017-11-14 10:59:57 -0600

    replaced HTML and stopped loader on page with movies

--- src/api.js
+++ src/api.js
@@ -2,5 +2,39 @@ module.exports = {
     getMovies: () => {
         return fetch('/api/movies')
             .then(response => response.json());
+            // .then(data => fillHTML());
     }
 };
+
+
+
+// movies.forEach(function (movie) {
+//     var moviesHTML = "";
+//
+//     moviesHTML += "<div class='row col-sm-offset-2 col-sm-8'>
+//     moviesHTML += "<div class='row col-sm-4'>"Movie"</div>";
+//     moviesHTML += "<div class='row col-sm-4'>"ID"</div>";
+//     moviesHTML += "<div class='row col-sm-4'>"Rating"</div>";
+//     moviesHTML += "<div class='row col-sm-4'>" + movie.title + "</div>";
+//     moviesHTML += "<div class='row col-sm-4'>" + movie.id + "</div>";
+//     moviesHTML += "<div class='row col-sm-4'>" + movie.rating + "</div>";
+//     moviesHTML += "</div>"
+//
+//     $(".bodyHTML").html(moviesHTML);
+//
+// }
+//
+// fillHTML();
+
+
+
+
+//
+// table +=
+// <tbody>
+// <tr>
+// <td class="id">${id}</td>
+//     <td class="rating">${rating}</td>
+//     <td class="title">${title}</td>
+//     </tr>
+//     </tboday>
