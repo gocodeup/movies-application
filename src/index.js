a85b281048c0a1dcd03b9dea3a7fd30637b06a95
Chanel Rodriguez <13car92@gmail.com>
2018-02-23 11:22:20 -0600

    switching point

--- src/index.js
+++ src/index.js
@@ -10,18 +10,32 @@
 const {getMovies} = require('./api.js');
 const $ = require('jquery');
 
-getMovies().then((movies) => {
-  $('#movieDisplay').html('Here are all the movies: ' + '<br>');
-  // console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-     // console.log(`id#${id} - ${title} - rating: ${rating}`);
-      $('#movieDisplay').append(`id#${id} - ${title} - rating: ${rating}<br>`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+function showMovies() {
+    getMovies().then((movies) => {
+        $('#movieDisplay').html('Here are all the movies: ' + '<br>');
+        // console.log('Here are all the movies:');
+        movies.forEach(({title, rating, id}) => {
+            // console.log(`id#${id} - ${title} - rating: ${rating}`);
+            $('#movieDisplay').append(`id#${id} - ${title} - rating: ${rating}<br>`);
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+}
+showMovies();
 
+// ###################    Check values for input  ##########################
+$('input').keyup(function(){
+  if($('#movieTitle').val()!=='' && $('#movieRating').val()!==''){
+    $('#addMovie').prop('disabled',false);
+  }else{
+      $('#addMovie').prop('disabled',true);
+  }
+})
+
+
+//#################    Add movie button  #####################################
 $('#addMovie').click(function(){
   event.preventDefault();
   let newMovie = {
@@ -36,5 +50,9 @@ $('#addMovie').click(function(){
       },
     body: JSON.stringify(newMovie)
   };
-  fetch(url,options);
+  fetch(url,options)
+      .then(showMovies);
+    $('#movieTitle').val('');
+    $('#movieRating').val('');
+
 });
