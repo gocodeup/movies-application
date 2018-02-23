225fcef8fdbe32c613365ece7138924e0cdd9413
Chris Kelley <kelleyc718@gmail.com>
2018-02-23 15:09:49 -0600

    Added styling

--- src/omdb.js
+++ src/omdb.js
@@ -4,7 +4,7 @@ const getMovies = (title) => {
   console.log(title);
   omdb.searchForMovie(title, {type: 'movie', r: 'json', page: 1}).then(results => {
     for (let i = 0; i < 10; i++) {
-      document.getElementById('poster-img').innerHTML += `<div class="results"><img src="${results.Search[i].Poster}"><h3>${results.Search[i].Title} ${results.Search[i].Year}</h3></div>`;
+      document.getElementById('poster-img').innerHTML += `<div class='results'><p>${results.Search[i].Title} ${results.Search[i].Year}</p><img src="${results.Search[i].Poster}"></div>`;
     }
   });
 };
