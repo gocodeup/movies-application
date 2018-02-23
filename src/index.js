744a476928276195c351c2e3e2733120bd5a3866
Chris Kelley <kelleyc718@gmail.com>
2018-02-23 16:44:02 -0600

    Added loading screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import getMovies from './omdb';
 
+setTimeout(() => {
+  document.querySelector('main').style.display = 'block';
+  document.getElementById('loading').style.display = 'none';
+}, 2000);
+
 let title = document.getElementById('searchVal');
 
 document.getElementById('searchVal').addEventListener('change', () => getMovies(title.value));
