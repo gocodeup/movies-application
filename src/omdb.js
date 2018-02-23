bd2698a8658f64a07f1a5c350606d2e0cc35746f
Chris Kelley <kelleyc718@gmail.com>
2018-02-23 15:45:28 -0600

    Styling changes to index

--- src/omdb.js
+++ src/omdb.js
@@ -2,11 +2,17 @@ const omdb = require('omdb-js')('544a671d');
 
 const getMovies = (title) => {
   console.log(title);
-  omdb.searchForMovie(title, {type: 'movie', r: 'json', page: 1}).then(results => {
-    for (let i = 0; i < 10; i++) {
-      document.getElementById('poster-img').innerHTML += `<div class='results'><p>${results.Search[i].Title} ${results.Search[i].Year}</p><img src="${results.Search[i].Poster}"></div>`;
-    }
-  });
+  omdb.searchForMovie(title, {
+    type: 'movie',
+    r: 'json',
+    page: 1,
+    plot: 'full'})
+    .then(results => console.log(results))
+    .then(results => {
+      for (let i = 0; i < 10; i++) {
+        document.getElementById('poster-img').innerHTML += `<div class='results'><p>${results.Search[i].Title} ${results.Search[i].Year}</p><img src="${results.Search[i].Poster}"></div>`;
+      }
+    });
 };
 
 export default getMovies;
