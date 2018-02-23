c741246d90f6660d6ae5a97ffe8d231101faf118
emmadejong <dejong.l.emma@gmail.com>
2018-02-23 15:05:25 -0600

    added functionality for page load and added form html

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ const $ = require('jquery');
  * es6 modules and imports
  */
 
-import loadingGif from './hello';
+// this runs the loading.gif image
+const loadingGif = () => {
+    $('.container').html("<img src='./img/page-loader.gif' class='loader'>");
+};
 loadingGif();
+
+// hide form until called in getMovies function
+$('.page-loader').hide();
 
 /**
  * require style imports
@@ -14,11 +20,27 @@ loadingGif();
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+  formLoader();
+  let moviesBuilder = [];
+  movies.forEach(({title, rating}) => {
+    moviesBuilder.push(`The title of the movie is: ${title} and the rating is: ${rating}.`);
   });
+
+// this builds movie html and prints to page
+let list = '<ul>';
+for (let mov of moviesBuilder) {
+  list += `<h2> ${mov} </h2>`;
+}
+list += '</ul>';
+$('.container').html(list);
+
+// this runs if there is an error
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
+// this function shows the forms upon page load when called
+function formLoader() {
+    $('.page-loader').show();
+}
