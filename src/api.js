d29d2314432ecaac227a75677de81a6fa08bca5d
Chris Kelley <kelleyc718@gmail.com>
2018-02-23 14:05:53 -0600

    OMDB connect to build

--- src/api.js
+++ src/api.js
@@ -1,13 +1,10 @@
 const omdb = require('omdb-js')('544a671d');
 
-const search = document.getElementById('searchVal').value();
-
-const getMovies = search => {
-  omdb.searchForMovie(search, {type: 'movie', r: 'json', page: 1}).then(results => {
+const getMovies = (title) => {
+  console.log(title);
+  omdb.searchForMovie(title, {type: 'movie', r: 'json', page: 1}).then(results => {
     console.log(results);
   });
 };
-
-document.addEventListener('oninput', getMovies());
 
 export default getMovies;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import getMovies from './api.js';
+import getMovies from './api';
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+let title = document.getElementById('searchVal');
+
+document.getElementById('searchVal').addEventListener('change', () => getMovies(title.value));
