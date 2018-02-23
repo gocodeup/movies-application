da362b8ca097e385f9ebcf888b9190794d19b0d3
Billy <billy_white.88@yahoo.com>
2018-02-23 12:33:15 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -8,13 +8,23 @@ sayHello('World');
  * require style imports
  */
 const {getMovies} = require('./api.js');
+let movieTitles = document.getElementById("movie-title");
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  console.log(movies);
+  movies.forEach(({title, rating}) => {
+      //   console.log(`id#${id} - ${title} - rating: ${rating}`);
+      movieTitles.innerHTML +=
+          ` 
+          <div>
+          ${title} - rating: ${rating}
+          </div>
+          
+          `;
+  })
+})
+  .catch((error) => {
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
