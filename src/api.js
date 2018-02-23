b675ad1791bfa20169436a489d7b8b08a2360bd1
Chris Kelley <kelleyc718@gmail.com>
2018-02-23 12:46:50 -0600

    change made to api

--- src/api.js
+++ src/api.js
@@ -1,9 +1,13 @@
 const omdb = require('omdb-js')('544a671d');
 
+const search = document.getElementById('searchVal').value();
+
 const getMovies = search => {
-  omdb.searchForMovie('matrix', {type: 'movie', r: 'json', page: 1}).then(results => {
+  omdb.searchForMovie(search, {type: 'movie', r: 'json', page: 1}).then(results => {
     console.log(results);
   });
 };
+
+document.addEventListener('oninput', getMovies());
 
 export default getMovies;
