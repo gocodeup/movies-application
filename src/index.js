6a0b1239c2592c11abf286da7c48603aca70e9f1
Chanel Rodriguez <13car92@gmail.com>
2018-02-23 10:58:51 -0600

    created add functionality

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
+// import sayHello from './hello';
+// sayHello('World');
 
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  $('#movieDisplay').html('Here are all the movies: ' + '<br>');
+  // console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+     // console.log(`id#${id} - ${title} - rating: ${rating}`);
+      $('#movieDisplay').append(`id#${id} - ${title} - rating: ${rating}<br>`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
+});
+
+$('#addMovie').click(function(){
+  event.preventDefault();
+  let newMovie = {
+    title: $('#movieTitle').val(),
+    rating: $('#movieRating').val()
+  };
+  const url = ('/api/movies');
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      },
+    body: JSON.stringify(newMovie)
+  };
+  fetch(url,options);
 });
