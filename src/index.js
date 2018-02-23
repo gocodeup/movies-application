316f38afca5c38da5558e7c829b679161b28af7f
davidalex620 <spand3r@gmail.com>
2018-02-23 11:36:11 -0600

    changing coders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+
+const $ = require('jquery');
+
 /**
  * es6 modules and imports
  */
@@ -9,6 +12,7 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
+
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
@@ -18,3 +22,9 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+// $('.container').html('Here are all the movies:');
+
+
+
+
