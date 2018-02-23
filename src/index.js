1708c048e23bb2948f4058505f49648278ba356d
zacharyramos <zachary.ramos9@gmail.com>
2018-02-23 16:46:44 -0600

    working delete button

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function updateMovies() {
       $(".newMovieForm").show();
       let html = "<table><tr><th>ID</th><th>Movie</th><th>Rating</th><th> </th></tr>";
       movies.forEach(({title, rating, id}) => {
-        html += `<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button value="${id}" class="edit">Edit</button></td></tr>`;
+        html += `<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button value="${id}" class="edit">Edit</button><button class="delete" value="${id}">Delete</button></td></tr>`;
       });
       html += "</table>";
       $(".container").html(html);
@@ -47,6 +47,10 @@ function updateMovies() {
              id = e.target.value;
             $(".editMovieForm").show();
         });
+        $(".delete").click((e) => {
+            id = e.target.value;
+            deleteMovie();
+        })
     }).catch((error) => {
       alert('Oh no! Something went wrong.\nCheck the console for details.');
       console.log(error);
@@ -78,4 +82,19 @@ function saveMovie() {
                 $("#newRating").val("1");
             })
             .catch();
+}
+
+function deleteMovie() {
+    // const movie = {title: $("#editMovie").val(), rating: $("#newRating").val(), id: ""};
+    const url = `/api/movies/${id}`;
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // body: JSON.stringify(movie)
+    };
+    fetch(url, options)
+        .then( () => updateMovies())
+        .catch();
 }
