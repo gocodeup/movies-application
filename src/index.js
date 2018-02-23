d58cc8c85f67bf0a216f91e78728f45726d75e41
lalepro <lauraleeprochaska@gmail.com>
2018-02-23 13:59:09 -0600

    page load section completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-/**
- * This file is the "entrypoint" into your application
- */
-import 'bootstrap'
-import $ from 'jquery'
 
-$(() => {
-  $('[data-toggle="popover"]').popover()
+const bootstrap = require('bootstrap');
+const $ = require('jquery');
+
+const loader = require('./page-load.js');
+const {getMovies} = require('./api.js');
+
+let movieTitles = document.getElementById("movie-title");
+
+getMovies().then((movies) => {
+    loader.showPage();
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating}) => {
+        // console.log(`id#${id} - ${title} - rating: ${rating}`);
+        movieTitles.innerHTML +=
+            `
+            <div>
+             ${title} - rating: ${rating} 
+             </div>
+            `;
+    })
 })
+    .catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+});
+
+
+
 
