f324705061f6768943141072c833261942c623a8
davidalex620 <spand3r@gmail.com>
2018-02-24 16:29:18 -0600

    got rid of BootStrap replaced with Flexbox, somestyleing changes

--- src/index.js
+++ src/index.js
@@ -14,15 +14,14 @@ const {getMovies} = require('./api.js');
 
 const runMovies = function() {
     getMovies().then((movies) => {
-        $("h1").text("Welcome to the movies");
         let htmlString = "";
         // console.log('Here are all the movies:');
         // movies.forEach(({title, rating, id}) => {
         //   console.log(`id#${id} - ${title} - rating: ${rating}`);
         movies.forEach(function (movie) {
-            htmlString += `<div class="panel panel-default"><h2>${movie.title}</h2></div> <div class="panel-body"><p>Rating ${movie.rating}</p></div>`
+            htmlString += `<div class="box box${movie.id}">${movie.id}. ${movie.title}</div> <div class="box box${movie.id}">Rating: ${movie.rating}</div>`
         });
-        $('.htmlHere').html(htmlString);
+        $('.container').html(htmlString);
 
     }).catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
@@ -32,7 +31,7 @@ const runMovies = function() {
 runMovies();
 
 const displayMovies = () => {
-    $('.main-display').html('Loading...');
+    $('.main').html('Loading...');
 };
 displayMovies();
 
@@ -42,7 +41,7 @@ displayMovies();
 $('#add-new-movie').click(function()  {
   const addMovie = $('#movieTitle').val();
   const addRating = $('#movieRating').val();
-  const addObj = {title: addMovie, body: addRating};
+  const addObj = {title: addMovie, rating: addRating};
   const url = '/api/movies';
   const options = {
     method: 'POST',
@@ -59,11 +58,11 @@ $('#add-new-movie').click(function()  {
         });
 });
 
-$('.panel-default').click(function() {
+$('h2').click(function() {
+    console.log("click");
   const id = (movies.id).val();
   console.log(id);
 });
-
 
 
 
