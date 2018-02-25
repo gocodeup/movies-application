1731150f62d2967c4bee222ded266bf3c8e51ba7
davidalex620 <spand3r@gmail.com>
2018-02-25 17:19:21 -0600

    rolled back to bootstrap

--- src/index.js
+++ src/index.js
@@ -11,17 +11,25 @@ const $ = require('jquery');
  */
 const {getMovies} = require('./api.js');
 
+//----------------------------------------------------
+// Movie list builder
+//----------------------------------------------------
+
 
 const runMovies = function() {
     getMovies().then((movies) => {
         let htmlString = "";
-        // console.log('Here are all the movies:');
-        // movies.forEach(({title, rating, id}) => {
-        //   console.log(`id#${id} - ${title} - rating: ${rating}`);
         movies.forEach(function (movie) {
-            htmlString += `<div class="box box${movie.id}">${movie.id}. ${movie.title}</div> <div class="box box${movie.id}">Rating: ${movie.rating}</div>`
+            htmlString += `<div class="col-xs-12" > <div class="row"><h5 class="col-xs-3">`;
+            htmlString += `${movie.title}`;
+            htmlString += `</h5>`;
+            htmlString += `<h6 class="col-xs-3 ratingCol">`;
+            htmlString += `Rating: ${movie.rating}`;
+            htmlString += `</h6>`;
+            htmlString += `<div class="col-xs-3"><button class="btn btn-info" type="button" id="editBtn${movie.id}">Edit</button><button class="btn btn-danger" type="button" id="deleteBtn${movie.id}">DELETE</button></div>`;
+            htmlString += `<div class="col-xs-12" id="editInputs${movie.id}"></div></div></div>`;
         });
-        $('.container').html(htmlString);
+        $('#display-movies').html(htmlString);
 
     }).catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
@@ -30,17 +38,26 @@ const runMovies = function() {
 };
 runMovies();
 
+//----------------------------------------------------
+// Display Loading while wait waiting for runMovies to build
+//----------------------------------------------------
+
 const displayMovies = () => {
-    $('.main').html('Loading...');
+    $('#display-movies').html('Loading...');
 };
 displayMovies();
 
 
-// on click of button pull in the values from id="movieTitle" id="movieRating"
 
-$('#add-new-movie').click(function()  {
-  const addMovie = $('#movieTitle').val();
-  const addRating = $('#movieRating').val();
+//----------------------------------------------------
+// on click of button pull in the values from id="movieTitle" id="movieRating"
+//----------------------------------------------------
+
+$('#submitBtn').click(function()  {
+  const addMovie = $('#movieTitleInput').val();
+  const addRating = $('#movieRatingInput').val();
+  console.log(addMovie);
+  console.log(addRating);
   const addObj = {title: addMovie, rating: addRating};
   const url = '/api/movies';
   const options = {
@@ -64,10 +81,38 @@ $('h2').click(function() {
   console.log(id);
 });
 
+//----------------------------------------------------
+// Delete Movie
+//----------------------------------------------------
 
 
-
-
+const deleteMovies = (movies) => {
+    movies.forEach((element) => {
+        $(`#deleteBtn${element.id}`).click(() => {
+            console.log(element.id);
+            let url;
+            let options;
+            if (confirm(`Are you Sure you want to delete:  ${element.title}?`)) {
+                url = `./api/movies/${element.id}`;
+                options = {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                };
+                fetch(url, options)
+                    .then(() => {
+                        console.log(`new edit function is firing`);
+                        fetchingMovieList();
+                    })
+                    .catch(() => {
+                        console.log('the upload failed')
+                    });
+            }
+        });
+    });
+};
+deleteMovies();
 
 
 
