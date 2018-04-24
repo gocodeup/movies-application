26d50672fc3a8c99603222d5c068e635eb840a20
heather arellano <hma.485@icloud.com>
2018-04-24 15:31:21 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@
 import 'bootstrap'
 import $ from 'jquery'
 
-$(() => {
-  $('[data-toggle="popover"]').popover()
-})
+// $(() => {
+//   $('[data-toggle="popover"]').popover()
+// })
 
+const {getMovies} = require('./api.js');
+
+getMovies().then((movies) => {
+    $(".x").toggleClass('invisible');
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+    });
+}).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
+});
