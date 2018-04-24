42b11f0d287d93996e49f0f91e636650f395daa6
Jake J <jakejohnson818@gmail.com>
2018-04-24 14:20:53 -0500

    Added loading message, appended movies to div instead of console.log

--- src/index.js
+++ src/index.js
@@ -7,14 +7,21 @@ sayHello('World');
 /**
  * require style imports
  */
+
+const $ = require("jquery");
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $(".container").append(`id#${id} - ${title} - rating: ${rating}`);
+    displayLoading();
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
+function displayLoading() {
+    $(".loadingMessage").css("display", "none")
+}
