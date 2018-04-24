a196fa60da8de6e67839b1a8f02c9e76bb3d4a25
Brian Torluemke <briantorluemke@gmail.com>
2018-04-24 15:56:59 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 /**
- * This file is the "entrypoint" into your application
+ * es6 modules and imports
  */
-import 'bootstrap'
-import $ from 'jquery'
+const $ = require('jquery');
+import sayHello from './hello.js';
+sayHello('World');
 
-$(() => {
-  $('[data-toggle="popover"]').popover()
-})
-
+/**
+ * require style imports
+ */
+const {getMovies} = require('./api.js');
+$(".loader").css("display", "block");
+getMovies().then((movies) => {
+    $(".loader").css("display", "none");
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+    });
+}).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+});
