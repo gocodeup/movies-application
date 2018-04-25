9dca9d0973b89a706ef923f7cc1229417d35f3ed
richardcalata <calatar6@gmail.com>
2018-04-25 09:09:30 -0500

    rearranged directories

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@
  */
 import 'bootstrap'
 import $ from 'jquery'
+//import '../movieStyle.css'
 
 $(() => {
   $('[data-toggle="popover"]').popover()
 })
 
+
+// index.js
+const data = require('../add_movie.js');
+
+console.log(data.whichSideOfTheForce); // outputs "The Dark Side"
+
+const message ="this message";
+
+console.log(message);
