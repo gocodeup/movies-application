4f72bd96bad6715fda1629981806e583bebf7707
danielconner <metalman1342@gmail.com>
2018-04-25 09:39:55 -0500

    added loading message, having issues initializing jquery

--- src/index.js
+++ src/index.js
@@ -15,9 +15,10 @@ getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $("table").append(`<tr><td>${id}</td><td>${title}</td><td>${rating}</td><td><button>delete</button></td></tr>`);
   });
 
 }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
 });
