1658b0eb5271c5b9110a0625213be5f0043d0504
Brian Torluemke <briantorluemke@gmail.com>
2018-04-25 15:13:57 -0500

    added functionality for creating cards taken from movie database.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-/**
- * es6 modules and imports
- */
+'use strict';
 const $ = require('jquery');
 import sayHello from './hello.js';
 sayHello('World');
 
-/**
- * require style imports
- */
 const {getMovies} = require('./api.js');
 $(".loader").css("display", "block");
 getMovies().then((movies) => {
     $(".loader").css("display", "none");
     console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
-        console.log(`id#${id} - ${title} - rating: ${rating}`);
+        $(".row").append(`<div class="col-4">
+       <div class="card">
+           <div class="card-body">
+               <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+               Edit
+               </button>
+               <button type="button" class="btn btn-danger">X</button>
+               <h5 class="card-title"><em>Movie Title: </em><br>${title}</h5>
+               <h6 class="card-subtitle"> ${rating} Stars</h6>
+               <p class="dbId">${id}</p>
+           </div>
+       </div>
+   </div>`);
     });
 }).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
