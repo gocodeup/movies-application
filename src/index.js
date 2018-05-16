1f9c1fb5b5df27cd9e1f7ae792202261d48b8832
Bolimbo <carlo.cappellini@yahoo.com>
2018-05-15 21:56:28 -0500

    was adding ID to json only had <header> instead of 'headers'

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ sayHello('World');
  */
 const getMovies = require('./api.js');
 
-table();
 
 function showAllMovies() {
     table();
@@ -35,41 +34,77 @@ showAllMovies();
 const header = new Headers({
     'Accept': 'application/json',
     'Content-Type': 'application/json'
-})
+});
 
-function table() {
-    document.getElementById("movie-list").innerHTML = `<tr><th>Movie Title</th><th>Movie Rating</th></tr>`
-
-}
-
-function displayTableMovies(title, rating) {
-    document.getElementById("movie-list").innerHTML +=`<tr><td>${title}</td><td>rating: ${rating}</td></tr>`
-
-}
 
 function addMovie() {
-    let movieTitle = document.getElementById('title').value;
-    let movieRating = document.getElementById('rating').value
+    let movieTitle = document.getElementById("title").value;
+    let movieRating = document.getElementById("rating").value;
+
     if (movieTitle === "") {
-        alert("its empty")
+        alert("It's Empty")
     } else {
         let movieObj = {
             title: movieTitle,
             rating: movieRating
         };
-        let options = {
+        let fetchOptions = {
             method: "POST",
             body: JSON.stringify(movieObj),
-            header: header
+            headers: header
         };
         document.getElementById("title").value = "";
         document.getElementById("rating").value = "1";
-        fetch("/api/movies", options)
-            .then(response => response.json())
+        fetch("/api/movies", fetchOptions)
+            .then((response) => console.log(response.json()))
         displayTableMovies(movieTitle, movieRating)
 
+
+
     }
+
 }
 
-
 document.getElementById("sub").addEventListener("click", addMovie)
+
+
+function table() {
+    document.getElementById("movie-list").innerHTML = `<tr><th>Movie Title</th><th>Movie Rating</th></tr>`
+
+
+}
+
+function displayTableMovies(title, rating) {
+    document.getElementById("movie-list").innerHTML += `<tr><td>${title}</td><td>rating: ${rating}</td></tr>`
+
+}
+
+function deleteButton() {
+    let movieToDeleteId = document.getElementById("delete-movie").value;
+    let fetchOptions = {
+        method: "DELETE",
+        headers: header
+    };
+    fetch(`/api/movies/${movieToDeleteId}`, fetchOptions).then(() => {
+        document.getElementById("movie-list").innerHTML = "";
+        unHide("movie-list-container");
+        reHide("delete-form");
+        unHide("new-movie-form");
+        unHide("delete-a-movie");
+        unHide("edit-button");
+        reHide("cancel-button");
+        disableButton("delete-this-movie")
+    })
+        .then(() => {
+            displayAllMovies()
+        })
+    // .then(()=>changeHelloAgain());
+}
+
+document.getElementById("delete-this-movie").addEventListener("click", deleteButton);
+document.getElementById("delete-this-movie").addEventListener("click", function () {
+    movieDeleted();
+    setInterval(changeHelloAgain, 1000)
+});
+
+
