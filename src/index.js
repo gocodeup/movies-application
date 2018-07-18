4f07ad62ee8cf6d06222d1c30e005167296bf42b
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-18 14:46:19 -0500

    create movie project

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@
 import sayHello from './hello';
 sayHello('World');
 
-/**
- * require style imports
- */
-const {getMovies} = require('./api.js');
-
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+// /**
+//  * require style imports
+//  */
+// const {getMovies} = require('./api.js');
+//
+// getMovies().then((movies) => {
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating, id}) => {
+//     console.log(`id#${id} - ${title} - rating: ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.')
+//   console.log(error);
+// });
