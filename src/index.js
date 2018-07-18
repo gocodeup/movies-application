bd2281f5f1da194e6cbf5f4bec4360bacd724117
MKovar <mkovar@mkovar.local>
2018-07-18 15:03:22 -0500

    loading feat complete

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ sayHello('World');
 /**
  * require style imports
  */
+
+
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
@@ -15,6 +17,25 @@ getMovies().then((movies) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
+});
+
+
+
+// $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+// $(window).on('load', function(){
+//     setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+// });
+// function removeLoader(){
+//     $( "#loadingDiv" ).fadeOut(500, function() {
+//         // fadeOut complete. Remove the loading div
+//         $( "#loadingDiv" ).remove(); //makes page more lightweight
+//     });
+// }
+
+
+$(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");
 });
