c680ee3bfbc38d884ad512ad2d08ea897f513b33
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-19 08:58:12 -0500

    fix fetch bug

--- src/hello.js
+++ src/hello.js
@@ -1,5 +1,6 @@
 export default (name) => console.log(`Hello there, ${name}!`);
+
 fetch("http://localhost:1313/index.html/db.json")
     .then(function(db){
-    return db.json()
+    return db();
     });
