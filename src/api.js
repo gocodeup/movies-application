ac6b320a0bec4290a52022726db919a83f9a3bf0
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-07-19 14:28:28 -0500

    added functionality to add movies button to json and clear out input boxes

--- src/api.js
+++ src/api.js
@@ -1,6 +1,17 @@
-module.exports = {
-    getMovies: () => {
-        return fetch('/api/movies')
-            .then(response => response.json())
-    }
+const getMovies = () => {
+    return fetch('/api/movies')
+        .then(response => response.json())
 };
+
+const addNewMovie = (addMovies) => {
+    const allMovies = '/api/movies';
+    const options = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(addMovies)
+    };
+    return fetch(allMovies, options)
+        .then(response => response.json());
+};
+
+module.exports = {getMovies, addNewMovie};
--- src/index.js
+++ src/index.js
@@ -6,37 +6,37 @@ sayHello('World');
 
 $("#myDiv").addClass('hide');
 
+const post = require('./api.js');
 
+$('#addMovie').click((e) => {
+    e.preventDefault();
+    const title = $('#title').val();
+    const rating = $('input[name=rating]:checked').val();
+    post.addNewMovie({title, rating});
+    setTimeout(function(){
+        moviesRefresh();
+    }, 1000);
+    $('input[type="text"], text').val('');
+    $('.form-group').find('input:radio').prop('checked', false);
+});
 
-
-function moviesRefresh(){
-    const userTitle = document.getElementById("title").value;
-    const userRating = document.getElementById("rating").value;
-    $('#movies').html("");
-    console.log(userTitle);
-    console.log(userRating);
-    $.getJSON("../db.json", function(data) {
-      let newMovie = {
-        // url: "",
-        // type: "POST",
-        // data: {
-          title: userTitle,
-            rating: userRating
-        };
-        data.push(newMovie);
-    }).done(function (data) {
-      console.log(data);
+function moviesRefresh() {
+    getMovies().then((movies) => {
+        $("#movies").html("");
+        console.log('Here are all the movies:');
+        console.log(movies);
         movies.forEach(({title, rating, id}) => {
-            $("#myDiv").append(`<div id="movies"> id# ${id} - ${title} - rating: ${rating}</div>`);
+            $("#movies").append(`<div>${title} - rating: ${rating}</div>`);
             console.log(`id#${id} - ${title} - rating: ${rating}`);
         });
-    })
-
+        $('#loader').addClass('hide');
+        $('#myDiv').removeClass('hide').addClass('show');
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
 }
-$("#addMovie").click(function(){
-  console.log("it clicked");
-  moviesRefresh();
-});
+
 /**
  * require style imports
  */
@@ -44,13 +44,14 @@ const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
     console.log('Here are all the movies:');
+    console.log(movies);
     movies.forEach(({title, rating, id}) => {
-        $("#myDiv").append(`<div id="movies"> id# ${id} - ${title} - rating: ${rating}</div>`);
+        $("#movies").append(`<div>${title} - rating: ${rating}</div> <button class="btn btn-outline-primary">Edit</button>`);
         console.log(`id#${id} - ${title} - rating: ${rating}`);
     });
     $('#loader').addClass('hide');
     $('#myDiv').removeClass('hide').addClass('show');
 }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
 });
