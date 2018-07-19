d82b57b5e6ea9bdc88a1727f2f0f080799b678c6
Aaron Trevino <Aaron@Aarons-MacBook-Pro.local>
2018-07-19 10:39:33 -0500

    updated repo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /**
  * es6 modules and imports
  */
+
+import $ from 'jquery';
+
 import sayHello from './hello';
 sayHello('World');
 
@@ -10,11 +13,37 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+    // console.log(`id#${id} - ${title} - rating: ${rating}`)
+
+
+    let htmlBuilder = '<table id="movie-list">';
+    htmlBuilder += '<thead>';
+    htmlBuilder += '<tr>';
+    htmlBuilder+= '<th>Film</th>';
+    htmlBuilder += '<th>Rating</th>';
+    htmlBuilder += '<th>Option</th>';
+    htmlBuilder += '</tr>';
+    htmlBuilder += '</thead>';
+
+    movies.forEach(({title, rating, id}) => {
+      //populate the table with json response data
+        htmlBuilder += `<tr><td id="tdTitle">${title}</td>`;
+        htmlBuilder += `<td id="tdStars">${rating}</td>`;
+        htmlBuilder += `<td id="tdOptions">`
+            +
+            `<i class="far fa-trash-alt" data-id="${id}"></i></td>` +
+            `<i class="far fa-trash-alt" data-id="${id}"></i></td>` +
+            `</tr>`
+
+
+
+    });
+    htmlBuilder += '</table>';
+    $('.container').html(htmlBuilder);
+
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+
