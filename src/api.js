6ae557a17d20608f8a33298d96897688b2d9255e
JRihn32 <justin.rihn32@gmail.com>
2018-07-19 09:48:43 -0500

    added form

--- src/api.js
+++ src/api.js
@@ -4,3 +4,15 @@ module.exports = {
       .then(response => response.json());
   }
 };
+document.onreadystatechange = function () {
+    var state = document.readyState
+    if (state == 'interactive') {
+        document.getElementById('contents').style.visibility="hidden";
+    } else if (state == 'complete') {
+        setTimeout(function(){
+            document.getElementById('interactive');
+            document.getElementById('load').style.visibility="hidden";
+            document.getElementById('contents').style.visibility="visible";
+        },1000);
+    }
+}
