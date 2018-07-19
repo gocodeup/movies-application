3b5bd0ed3d4792260e7e1ab6bc0d76e2c46c345e
Don Pho <donpho@Dons-MacBook-Air.local>
2018-07-19 14:25:00 -0500

    added movies and trying to prevent default screen

--- src/index.js
+++ src/index.js
@@ -7,21 +7,43 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
-const $ = require('jquery');
 
-getMovies().then((movies) => {
-  $('#loading').hide();
-  console.log('Here are all the movies:');
-    $('#movieList').html("");
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+function runMovie() {
+    const {getMovies} = require('./api.js');
+    const $ = require('jquery');
 
-      $('#movieList').append(`<div id = ${id}><p><strong>Title:</strong> ${title}</p>
+    getMovies().then((movies) => {
+        $('#loading').hide();
+        console.log('Here are all the movies:');
+        $('#movieList').html("");
+        movies.forEach(({title, rating, id}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+            $('#movieList').append(`<div id = ${id}><p><strong>Title:</strong> ${title}</p>
       <strong>Rating:</strong> ${rating}</div>`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+}
+runMovie();
+
+
+// $("#addMovie").submit(function() {
+//     $(this).preventDefault();
+// });
+
+$('#submit').click( e => {
+  e.preventDefault();
+
+  let title = $('#movieTitle').val();
+  let rating = $('#rating').val();
+
+  $.post("/api/movies", {
+    title: title,
+    rating: rating
+  }).complete( () => console.log("Success!"))
+
 });
 
