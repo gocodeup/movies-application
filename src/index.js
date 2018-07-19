8421edec0ee1986fc926ffc8eff60312594e1ffe
Amos Davis <amos.davis367@gmail.com>
2018-07-19 10:56:14 -0500

    Completed - remove 'loading message and replaced with ajax request onto html

--- src/index.js
+++ src/index.js
@@ -8,13 +8,20 @@ sayHello('World');
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
 
 getMovies().then((movies) => {
+  $('#loading').hide();
   console.log('Here are all the movies:');
+    $('.container').html("");
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+
+      $('.container').append(`<div id = ${id}><p><strong>Title:</strong> ${title}</p>
+      <strong>Rating:</strong> ${rating}</div>`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
