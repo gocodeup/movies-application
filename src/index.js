16d27ed33064fd4dc6171ef18c926f831002a192
Don Pho <donpho@Dons-MacBook-Air.local>
2018-07-19 09:40:05 -0500

    solved the problem for a loading screen

--- src/index.js
+++ src/index.js
@@ -8,8 +8,10 @@ sayHello('World');
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
 
 getMovies().then((movies) => {
+  $('#loading').hide();
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
@@ -18,3 +20,4 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
