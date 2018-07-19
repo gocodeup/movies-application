934012f73a9e4a8676fcc75b7c0ed8872f9d5dba
Tylor Pelletier <tylorpelletier@Tylors-MacBook-Pro.local>
2018-07-19 10:44:17 -0500

    added loading screen and api info to webpage

--- src/index.js
+++ src/index.js
@@ -4,17 +4,40 @@
 import sayHello from './hello';
 sayHello('World');
 
+$(function () {
+    let isItLoaded = $("#myDiv");
+
+    setTimeout(function () {
+        if (isItLoaded == undefined) {
+            $("#loader").css("display", "block")
+        } else if (isItLoaded !== undefined) {
+            showPage();
+        }
+    }, 3000);
+
+// let myVar;
+// $(function myFunction() {
+//     myVar = setTimeout(showPage, 3000);
+// });
+
+    function showPage() {
+        $("#loader").css("display", "none");
+        $("#myDiv").css("display", "block");
+    }
+});
+
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        $("#myDiv").append(`id#${id} - ${title} - rating: ${rating}`);
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+    });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.')
+    console.log(error);
 });
