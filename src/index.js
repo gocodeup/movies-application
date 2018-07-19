d5d8dfac6facb986266fca058a63398e84e1f326
Christian Kenney <armyystrongg@Christians-MacBook-Air.local>
2018-07-19 12:34:26 -0500

    updated loading, started adding movies form

--- src/index.js
+++ src/index.js
@@ -4,28 +4,39 @@
 import sayHello from './hello';
 sayHello('World');
 
-$(function () {
-    let isItLoaded = $("#myDiv");
+$("#myDiv").addClass('hide');
 
-    setTimeout(function () {
-        if (isItLoaded == undefined) {
-            $("#loader").css("display", "block")
-        } else if (isItLoaded !== undefined) {
-            showPage();
-        }
-    }, 3000);
 
-// let myVar;
-// $(function myFunction() {
-//     myVar = setTimeout(showPage, 3000);
-// });
 
-    function showPage() {
-        $("#loader").css("display", "none");
-        $("#myDiv").css("display", "block");
-    }
+
+function moviesRefresh(){
+    const userTitle = document.getElementById("title").value;
+    const userRating = document.getElementById("rating").value;
+    $('#movies').html("");
+    console.log(userTitle);
+    console.log(userRating);
+    $.getJSON("../db.json", function(data) {
+      let newMovie = {
+        // url: "",
+        // type: "POST",
+        // data: {
+          title: userTitle,
+            rating: userRating
+        };
+        data.push(newMovie);
+    }).done(function (data) {
+      console.log(data);
+        movies.forEach(({title, rating, id}) => {
+            $("#myDiv").append(`<div id="movies"> id# ${id} - ${title} - rating: ${rating}</div>`);
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+        });
+    })
+
+}
+$("#addMovie").click(function(){
+  console.log("it clicked");
+  moviesRefresh();
 });
-
 /**
  * require style imports
  */
@@ -34,9 +45,11 @@ const {getMovies} = require('./api.js');
 getMovies().then((movies) => {
     console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
-        $("#myDiv").append(`id#${id} - ${title} - rating: ${rating}`);
+        $("#myDiv").append(`<div id="movies"> id# ${id} - ${title} - rating: ${rating}</div>`);
         console.log(`id#${id} - ${title} - rating: ${rating}`);
     });
+    $('#loader').addClass('hide');
+    $('#myDiv').removeClass('hide').addClass('show');
 }).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.')
     console.log(error);
