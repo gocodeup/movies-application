b5f9b6bbe0096392daf2914682c454618dc7e636
Natalie Mosqueda <natalie_bustos18@Natalies-MacBook-Pro-5.local>
2018-07-19 10:41:34 -0500

    form button - added 1 movie

--- src/index.js
+++ src/index.js
@@ -27,21 +27,54 @@ function buildHtml(arrOfObj) {
         html += "</tr>";
     });
     html += "</table>";
+
     html += "<form>";
     html += "<label>Movie Title</label>";
-    html += "<input>";
+    html += "<input id='movieTitle'>";
     html += "<label>Movie Rating</label>";
-    html += "<input>";
+    html += "<input id='movieRating'>";
     html += "</form>";
-    html += "<button id='submitButton' type='submit'>Submit</button>";
+    html += "<button id='submitButton'>Submit</button>";
     return html;
 }
 
+let addMovie = () => {
+    $('#submitButton').click(() => {
+        let movieTitleVal = $('#movieTitle').val();
+        let movieRatinfVal = $('#movieRating').val();
+
+        let newMovie = {title:movieTitleVal, rating:movieRatinfVal};
+        let url = '/api/movies';
+        let options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovie),
+        };
+
+        fetch(url, options)
+            .then(() => {
+                getMovies().then((data) => $(".JsonTable").html(buildHtml(data)));
+                console.log("1");
+            })
+            .catch(/* handle errors */);
+    });
+};
 
 
 getMovies().then((data) => $(".JsonTable").html(buildHtml(data)))
     .then(() => $(".container").hide())
+    .then(() => addMovie())
     .catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
         console.log(error);
     });
+
+
+
+$('#submitBtn2').click(() => {
+    $('body').css('background-color', 'red');
+
+});
+
