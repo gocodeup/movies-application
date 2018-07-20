ec13e708927d0fff32f4dec0df64ccce1d1c6133
Natalie Mosqueda <natalie_bustos18@Natalies-MacBook-Pro-5.local>
2018-07-20 10:25:21 -0500

    Still working on edit movies

--- src/index.js
+++ src/index.js
@@ -22,16 +22,19 @@ function buildHtml(arrOfObj) {
     html += "<th>Movie Name</th>";
     html += "<th>Movie Rating</th>";
     html += "<th>Movie ID</th>";
+    html += "</tr>";
     arrOfObj.forEach((movie) => {
         html += "<tr>";
-        html += "<td><button class='editBtns'>Edit</button><button>Delete</button>" + movie.title + "</td>";
+        html += "<td>" + movie.title + "</td>";
         html += "<td>" + movie.rating + "</td>";
         html += "<td>" + movie.id + "</td>";
+        html += "<td class='editCol'><button class='editBtns'>Edit</button><a href='#'><i class='fa fa-trash-o' style='font-size:24px'></i></a></td>";
         html += "</tr>";
         html += "<tr class='editRow'>";
-        html += "<td><input id='editMovie'></td>";
+        html += `<td><input data-id=${movie.id}></td>`;
         html += "<td><input id='editRating'></td>";
-        html += "<td><button class='saveBtns'>Save</button>";
+        html += "<td></td>";
+        html += "<td><i class='saveBtns fa fa-save' style='font-size:24px'></i></td>";
         html += "</tr>";
     });
     html += "</table>";
@@ -50,9 +53,9 @@ getMovies().then((data) => $(".JsonTable").html(buildHtml(data)))
 let addMovie = () => {
     $('#test').click(() => {
         let movieTitleVal = $('#movieTitle').val();
-        let movieRatinfVal = $('#movieRating').val();
+        let movieRatingVal = $('#movieRating').val();
 
-        let newMovie = {title:movieTitleVal, rating:movieRatinfVal};
+        let newMovie = {title:movieTitleVal, rating:movieRatingVal};
         let url = '/api/movies';
         let options = {
             method: 'POST',
@@ -75,17 +78,54 @@ let editMovie = () => {
     });
 };
 
+// let updateMoveAfterEdit = () => {
+//     $(document).on('click', '.saveBtns', (e) => {
+//         console.log($(e.currentTarget).last());
+//     });
+// };
+
 let updateMoveAfterEdit = () => {
     $(document).on('click', '.saveBtns', (e) => {
-        console.log($(e.currentTarget).parent().parent().prev())
+        //console.log($(e.currentTarget).parent().parent().prev().children().last().html()); //GETS MOVIE ID
+        console.log($(e.currentTarget).parent().parent().children().first().val());
+        let movieId = parseInt($(e.currentTarget).parent().parent().prev().children().next().next().html());
+
+       // console.log($(e.currentTarget).parent().parent().prev().children().html());
+        // let editRating = $(e.currentTarget).parent().parent().prev().children().next().html();
+        let editTitle = $('#editMovie').val();
+        let editRating = $('#editRating').val();
+        let newMovie = {title: editTitle, rating:editRating};
+        let url = `/api/movies/${movieId}`;
+        let options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newMovie),
+        };
+        fetch(url, options)
+            .then(() => {
+                getMovies().then((data) => $(".JsonTable").html(buildHtml(data)));
+            })
+            .catch(() => console.log("error!"));
+
+        // let newMovie = {title:editTitle, rating:editRating};
+        // let request = $.ajax({
+        //     url: "/api/movies",
+        //     method: "POST",
+        //     data: { title: editTitle, rating: editRating },
+        //     dataType: "html"
+        // });
+        //
+        // fetch(request)
+        //     .then(() => {
+        //         getMovies().then((data) => $(".JsonTable").html(buildHtml(data)));
+        //     })
+        //     .catch(() => console.log("error!"));
     });
-
-
-
 };
 
 updateMoveAfterEdit();
-
 
 addMovie();
 editMovie();
