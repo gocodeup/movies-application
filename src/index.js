79f49fa58b440ba763df77584bb7873ce92eecc3
Don Pho <donpho@Dons-MacBook-Air.local>
2018-07-20 13:59:58 -0500

    added a half functioning edit function

--- src/index.js
+++ src/index.js
@@ -21,20 +21,43 @@ function runMovie() {
         movies.forEach(({title, rating, id}) => {
             console.log(`id#${id} - ${title} - rating: ${rating}`);
 
-            $('#movieList').append(`<div id = ${id}><p><strong>Title:</strong> ${title} </p>
-      <strong>Rating:</strong> ${rating}<p><input type="text" name="title" value="${title}"></p><button>Edit</button><button>Delete</button></div><br>`);
-        });
-    }).catch((error) => {
-        alert('Oh no! Something went wrong.\nCheck the console for details.')
-        console.log(error);
+            $('#movieList').append(`<div id = ${id}><p><strong>Title #${id}:</strong> ${title} </p>
+      <strong>Rating:</strong> ${rating}<br>
+`);
+        },
+        $('#editMovie').html(`Select Movie Number to Edit <br>
+        <input id="inputID" type="number" min="1" max="${movies.length}" value="1">
+        <br>Title<br><input type="text" id="editTitle" name="title">
+        <br>Rating<br><input id="editRating" type="number" name="rating" min="1" max="5" value="1">
+        <br><button id="editBtn">Edit</button>
+`),
+            $('#editBtn').click(e => {
+                e.preventDefault();
+    //             let editTitle = $('#editTitle').val();
+    //             let editRating = $('#editRating').val();
+    //             let selectedID = $('#inputID').val();
+    //
+    //             let editMovie ={ title: editTitle, rating: editRating};
+    //             console.log(editMovie);
+    //             console.log(JSON.stringify(editMovie));
+    //
+    //             $.ajax({
+    //                 url:   `/api/movies/${selectedID}`,
+    //                 type: 'PUT',
+    //                 data: JSON.stringify(editMovie),
+    //                 contentType: 'json'
+    //             });
+    //             runMovie();
+    //         })
+    //     );
+    // }).catch((error) => {
+    //     alert('Oh no! Something went wrong.\nCheck the console for details.');
+    //     console.log(error);
+
+
     });
 }
 runMovie();
-
-
-// $("#addMovie").submit(function() {
-//     $(this).preventDefault();
-// });
 
 $('#submit').click( e => {
   e.preventDefault();
@@ -49,4 +72,10 @@ $('#submit').click( e => {
     runMovie();
 
 });
+
+/*
+*
+*   <p><input type="text" name="title" value="${title}"></p>
+    <button class="editBtn">Edit</button><button>Delete</button></div><br>
+* */
 
