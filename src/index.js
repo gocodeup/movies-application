8650f0acbc5ed7e30ce9d40e2f0ee1a216086e1a
Jillian Wysoki <jillianwysoki@gmail.com>
2018-07-22 16:23:51 -0500

    made style and button functionality changes, added drop down list option

--- src/index.js
+++ src/index.js
@@ -5,16 +5,25 @@ const {getMovies} = require('./api.js');
 require('bootstrap');
 const $ = require('jquery');
 
+
+/* XAVIER: in order to show the loading screen inbetween actions, you will need to write a function that replaces the body
+of your html with the loading screen. Run that function while the api call is taking place.
+ */
+
+
 // Display all movies
 const showMovies = () => {
     getMovies().then((movies) => {
         $('#t-body').empty();
 
-        movies.forEach(({title, rating, id}) => {
+        movies.forEach(({title, rating, genre, id}) => {
             const movies = ('<tr>' +
-                            '</tr><th scope="row">' + `${title}` + '</th>' +
+                            '</tr><td scope="row">' + `${title}` + '</td>' +
                             '<td>' + `${rating}` + '</td>' +
-                            '<td><button type="button" class="btn btn-primary edit-buttons" data-toggle="modal" data-target="#edit-module" id="'+`${id}`+'">Edit</button> ' + ' <button type="button" id="'+`${id}`+'" class="btn btn-danger delete-buttons">Delete</button></td>' +
+                            '<td>' + `${genre}` + '</td>' +
+                            '<td><button type="button" class="btn btn-primary edit-buttons" data-toggle="modal" data-target="#edit-module"                              id="'+`${id}`+'"><i class="fas fa-pencil-alt"></i>' +
+                '</button> ' + ' <button type="button" id="'+`${id}`+'" class="btn btn-danger delete-buttons"><i class="fas fa-trash-alt"></i>' +
+                '</button></td>' +
                             '</tr>'
             );
             $('#t-body:last').append(movies);
@@ -42,10 +51,12 @@ const showMovies = () => {
                     $('#edit-movie-id').val(movie.id);
                     $('#edit-movie-title').val(movie.title);
                     $('#edit-movie-rating').val(movie.rating);
+                    $('#edit-movie-genre').val(movie.genre);
                 });
         });
         //DELETE
         $('.delete-buttons').click((button) => {
+            $('.btn').attr('disabled',true);
             const url = `/api/movies/${button.target.id}`;
             const options = {
                 method: 'DELETE',
@@ -56,6 +67,7 @@ const showMovies = () => {
 
             fetch(url, options)
                 .then(showMovies).then(() => {
+                $('.btn').attr('disabled',false);
                 button.target.parentNode.parentNode.remove()
             })
         })
@@ -66,10 +78,12 @@ const showMovies = () => {
     });
 };
 
-// New movie submit
+// ADD MOVIE (New movie submit)
 $('#add-movies').click((e) => {
+
+
     e.preventDefault();
-    const newMovie = {title: $('#add-movie-title').val(), rating: $('#add-movie-rating').val()};
+    const newMovie = {title: $('#add-movie-title').val(), rating: $('#add-movie-rating').val(), genre: $('#add-movie-genre').val()};
     const url = '/api/movies/';
     const options = {
         method: 'POST',
@@ -79,18 +93,21 @@ $('#add-movies').click((e) => {
         body: JSON.stringify(newMovie)
     };
 
-    fetch(url, options)
+
+    return fetch(url, options)
         .then(showMovies).then(() => {
         $('#add-movie-title').val("");
         $('#add-movie-rating').val("");
-    });
+        $('#add-movie-genre').val("");
+
+        });
 });
 
 
 // Save changes button, in edit module
 $('#save').click((e) => {
     e.preventDefault();
-    const editMovies = {title: $('#edit-movie-title').val(), rating: $('#edit-movie-rating').val()};
+    const editMovies = {title: $('#edit-movie-title').val(), rating: $('#edit-movie-rating').val(), genre: $('#edit-movie-genre').val()};
     const url = `/api/movies/${$("#edit-movie-id").val()}`;
     const options = {
         method: 'PUT',
