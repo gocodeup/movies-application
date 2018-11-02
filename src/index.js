815dc5011502052bbd7b2f11e263fd281d4d7c37
KennethMcInturf <kennethcmcinturf@gmail.com>
2018-11-02 14:26:08 -0500

    can add movies

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const $ = require('jquery');
+
+
 /**
  * es6 modules and imports
  */
@@ -9,12 +12,35 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+
+const populateMovies = getMovies().then((movies) => {
+  $('#loading').remove();
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $('#container').append(`<h1>${title}</h1>`);
+    $('#container').append(`<p>${rating}</p>`)
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+$('#addMovie').click(function () {
+  const newMovieTitle = $('#newMovieTitle').val();
+  const newMovieRating = $('#newMovieRating').val();
+    const newMovie = {title: newMovieTitle, rating: newMovieRating};
+    const url = './api/movies';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    };
+    fetch(url, options)
+        .then(data => {
+            console.log(data);
+            getMovies();
+        })
+        .catch();
+})
+
