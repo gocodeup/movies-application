9af8be3bccd52fddbde9882f7ab0c0d2cb007c98
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2018-12-21 14:12:55 -0600

    Need help defining undefined functions

--- src/index.js
+++ src/index.js
@@ -14,15 +14,7 @@ const {
   getMovie,
   deleteMovie
 } = require('./api.js');
-// patchMovie({
-//     "title": "movie",
-//     "rating": "4"
-// }, 1);
-// postMovie( {
-//     "title": "movie",
-//     "rating": "4",
-//     "id": 1
-// });
+
 getMovies().then(movies => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
@@ -32,15 +24,19 @@ getMovies().then(movies => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
-// const addMovies = (response) => {
-//   $('#searchButton').on('click', function(){
-//     $('#addMovie').append()
-//   })
-// };
-getMovie(2);
 
-patchMovie({"title":"some movie", "rating":"3"}, 3);
+function test_getMovie() {
+  getMovie(3000);
+}
 
-patchMovie({"title":"Star Wars VII", "rating":"0.5"}, 4);
+const test_patchMovie = () => {
 
-deleteMovie(4);
+}
+
+const test_postMovie = () => {
+
+}
+
+const test_deleteMovie = () => {
+
+}
