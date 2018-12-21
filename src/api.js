5ce0558448deedd31959ffaef7767e0a941017ea
Justin <justin@Justins-MacBook-Pro.local>
2018-12-21 11:58:43 -0600

    added functions for patchMovie and postMovie, added some html

--- src/api.js
+++ src/api.js
@@ -2,5 +2,31 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+    postMovie: (data) => {
+        return fetch("/api/movies",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: JSON.stringify(data)
+        })
+            .then(response => {
+                console.log(response.json());
+            });
+    },
+    patchMovie: (data, id) => {
+    fetch(`/api/movies/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => {
+            console.log(response.json());
+        })
   }
 };
--- src/index.js
+++ src/index.js
@@ -7,14 +7,27 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
-
+const {getMovies, patchMovie, postMovie} = require('./api.js');
+// patchMovie({
+//     "title": "movie",
+//     "rating": "4"
+// }, 1);
+// postMovie( {
+//     "title": "movie",
+//     "rating": "4",
+//     "id": 1
+// });
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+const addMovies = (response) => {
+  $('#searchButton').on('click', function(){
+    $('#addMovie').append()
+  })
+};
