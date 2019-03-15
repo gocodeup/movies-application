3c6f7cae6ca2be1a48a289db14d9d194281a7012
Mike Flores <mike.flores3717@gmail.com>
2019-03-15 11:50:06 -0500

    adding finalized logic to that guy

--- src/index.js
+++ src/index.js
@@ -9,22 +9,25 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 let buttons = document.getElementsByClassName('remove');
+let editButtons = document.getElementsByClassName('edit');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   var output = '';
   movies.forEach(({title, rating, id}) => {
-    output += `<button class="remove" id="${id}">Delete</button>${title} - rating: ${rating}<br>`;
+    output += `<button class="edit" id="${id}">Edit</button><button class="remove" id="${id}">Delete</button>${title} - rating: ${rating}<br>`;
     document.getElementById('output').innerHTML = output;
     for (var i = 0; i < buttons.length; i++) {
       buttons[i].addEventListener('click', remove);
+    }
+    for (var i = 0; i < editButtons.length; i++) {
+          editButtons[i].addEventListener('click', populateEditForm);
     }
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
-
 
  document.getElementById('submit').addEventListener('click', submit1);
 
@@ -45,27 +48,26 @@ getMovies().then((movies) => {
      body: JSON.stringify(data)
    })
        .then((data) => {
-         reloaded();
          console.log('Request success: ', data);
        })
        .catch((error) => {
          console.log('Request failure: ', error);
        });
+
+     setTimeout(reloadTheObjects, 500);
  }
 
- function reloaded() {
-   fetch('http://localhost:3000/movies').then((response) => {
-     let data = response.json().then((data) => {
-       let output = '';
-       for(let i = 0; i < data.length; i++) {
-         output += data[i].title + ' ' + data[i].rating + '<br>';
-       }
-       document.getElementById('output').innerHTML = output;
-     })
-   })
- }
-
-
+ // function reloaded() {
+ //   fetch('http://localhost:3000/movies').then((response) => {
+ //     let data = response.json().then((data) => {
+ //       let output = '';
+ //       for(let i = 0; i < data.length; i++) {
+ //         output += data[i].title + ' ' + data[i].rating + '<br>';
+ //       }
+ //       document.getElementById('output').innerHTML = output;
+ //     })
+ //   })
+ // }
 
  function test () {
    console.log("test");
@@ -75,8 +77,75 @@ function remove(event) {
     fetch('http://localhost:3000/movies/' + event.target.id, {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json' }
-        })
+        });
+
+    setTimeout(reloadTheObjects, 500);
 }
 
+function populateEditForm(event) {
+    console.log(event.target);
+    let html = '';
+    getMovies().then((movies) => {
+        console.log(movies);
+        var output = '';
+            for (let i = 0; i < movies.length; i++) {
+                if (event.target.id == movies[i].id)
+                    document.getElementById('editForm').innerHTML = '<form>' +
+                        '<label for="editMovie">' + 'Change title for: ' + movies[i].title + '</label>\n' +
+                        '<input id="editMovie" placeholder=' + 'title' + '>' +
+                        '<label for="editRating">' + "Change current rating: " + movies[i].rating + '</label>' +
+                        '<input id="editRating" placeholder=' + 'rating' + '>' +
+                        '<input id="editId" type="hidden" value=' + movies[i].id + '>' +
+                        '<input id="editPostSubmit" type="submit">' +
+                        '</form>'
+            }
+            document.getElementById("editPostSubmit").addEventListener('click', editMovieObjects);
 
+
+    })
+ }
+
+function reloadTheObjects() {
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        var output = '';
+        movies.forEach(({title, rating, id}) => {
+            output += `<button class="edit" id="${id}">Edit</button><button class="remove" id="${id}">Delete</button>${title} - rating: ${rating}<br>`;
+            document.getElementById('output').innerHTML = output;
+            for (var i = 0; i < buttons.length; i++) {
+                buttons[i].addEventListener('click', remove);
+            }
+            for (var i = 0; i < editButtons.length; i++) {
+                editButtons[i].addEventListener('click', populateEditForm);
+            }
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.')
+        console.log(error);
+    });
+}
+
+function editMovieObjects(e) {
+    console.log(e);
+    e.preventDefault();
+    let movie = document.getElementById('editMovie').value;
+    let rating = document.getElementById('editRating').value;
+    let id = document.getElementById('editId').value;
+
+    console.log(movie);
+    console.log(rating);
+    console.log(id);
+
+    let data = {
+        title: movie,
+        rating: rating
+    };
+    fetch('http://localhost:3000/movies/' + id, {
+        method: 'PUT',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    setTimeout(reloadTheObjects, 500);
+}
 
