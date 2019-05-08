f1bbabee3b6ee7d0c0f098c372a5a19174564cb6
Nathaniel Dowell <dowellnathaniel@yahoo.com>
2019-05-08 16:52:07 -0500

    can add new movie to database, working on loading thing

--- src/api.js
+++ src/api.js
@@ -2,5 +2,20 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+
+  addNewMovie: (movieTitle, rating) => {
+    const newMovie = {title: movieTitle, rating: rating};
+    const url = '/api/movies';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newMovie),
+    };
+    fetch(url, options)
+        .then(/* post was created successfully */)
+        .catch(/* handle errors */);
   }
 };
