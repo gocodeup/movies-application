d0ef5cc819a519f05833fbcca8f58b4e8590b4c4
Edwin Buendia <edwinbuendia@Edwins-MacBook-Pro.local>
2019-05-09 16:49:56 -0500

    added buttons and display movie function

--- src/api.js
+++ src/api.js
@@ -4,6 +4,11 @@ module.exports = {
       .then(response => response.json());
   },
 
+  displayMovies: () => {
+    return fetch('/api/movies')
+        .then(response => response.json());
+  },
+
   addNewMovie: (movieTitle, rating) => {
     const newMovie = {title: movieTitle, rating: rating};
     const url = '/api/movies';
@@ -18,4 +23,19 @@ module.exports = {
         .then(/* post was created successfully */)
         .catch(/* handle errors */);
   }
+
+  // deleteMovie: () => {
+  //   const movie = {title: "", rating: ""};
+  //   const url = '/api/movies';
+  //   const options = {
+  //     method: 'DELETE',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(movie),
+  //   };
+  //   fetch(url, options)
+  //       .then(/* post was created successfully */)
+  //       .catch(/* handle errors */);
+  // }
 };
