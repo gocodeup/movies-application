50548a89aa9a61f88f14fbdf62b0e9b459701e21
Edwin Buendia <edwinbuendia@Edwins-MacBook-Pro.local>
2019-05-09 11:55:55 -0500

    added new branch

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ sayHello('World');
 /**
  * require style imports
  */
+<<<<<<< Updated upstream
 const {getMovies} = require('./api.js');
+=======
+
+const $ = require('jquery');
+
+
+const {getMovies, addNewMovie, deleteMovie, displayMovies} = require('./api.js');
+>>>>>>> Stashed changes
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
@@ -18,3 +26,23 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+<<<<<<< Updated upstream
+=======
+
+$('#add-movie').click(function(event){
+  //prevents the page from refreshing
+  event.preventDefault();
+
+  // //mmmm...store the value of the text inputs into variables
+    let movieTitle = $("#movie-title").val();
+    let rating = $("#movie-rating").val();
+    addNewMovie(movieTitle, rating);
+  // // call addMovies, passing in those variables
+
+});
+
+$("#deleteBtn").on("click", function () {
+  alert("click");
+//  code to send Delete request
+});
+>>>>>>> Stashed changes
