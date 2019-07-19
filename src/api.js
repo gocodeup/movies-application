b5cdcb95db4b3320be3a67a07909906028f8b501
Steven <steven.turner12x@gmail.com>
2019-07-19 16:35:59 -0500

    first

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
-module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
-  }
-};
+// module.exports = {
+//   getMovies: () => {
+//     return
+    fetch('https://api.themoviedb.org/3/movie/550?api_key=',{headers: {'Authorization':'token ' + tmdbKey}})
+    .then(response => response.json())
+        .then(data => console.log(data));
+  //
+
+// };
+
+
