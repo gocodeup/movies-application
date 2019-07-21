cf74ac418022d051ba7aa742ee920c76a5ec9717
Steven <steven.turner12x@gmail.com>
2019-07-21 13:05:07 -0500

    fetch is finally returning a promise

--- src/movieSearch.js
+++ src/movieSearch.js
@@ -15,15 +15,28 @@ const {getMovies} = require('./api.js');
 
 const omdbKey = "aefabb3f";
 // var searchedMovie;
+var movies
 $('#btn-1').on("click", function () {
 	let input = $('#searchText');
 	return fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${omdbKey}&s=${input.val()}&type=movie`)
 		.then(results => results.json())
-		.then(data => console.log(data))
-		.then(function (data) {
-			const movies = data;
-			for (let movie of Array.from(movies)) {
-				return movie.title
+		.then(data => movies = data)
+		.then(() => {
+			console.log(movies)
+		})
+		.then(() => {
+			for (let movie of movies) {
+				return movie.Title
+				console.log(movie.Title)
 			}
 		})
+
 })
+
+
+	// .then(function (data) {
+	// 	const movies = data;
+	// 	for (let movie of Array.from(movies)) {
+	// 		return movie.title
+	// 	}
+	// })
