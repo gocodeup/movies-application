e05de70e881a14c0622c3575070c63abc6ca8b31
Christopher Caldwell <christophercaldwell@IT-33756.attlocal.net>
2019-07-24 11:20:51 -0500

    going back to original

--- src/api.js
+++ src/api.js
@@ -1,42 +1,5 @@
 module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
-  },
-    createMovie: (content) => {
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(content),
-        };
-        console.log(content);
-        event.preventDefault();
-        return fetch('/api/movies', options)
-            .then(response => response.json())
-    },
-    editMovie: (movie, id) => {
-        const options = {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(movie),
-        };
-        return fetch(`/api/movies/${id}`, options)
-            .then(response => response.json())
-  },
-    deleteMovie: (movie, id) => {
-        const options = {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(movie),
-        };
-        return fetch(`/api/movies/${id}`, options)
-            .then(response => response.json())
-    }
-
+	getMovies: () => {
+		return fetch("/api/movies").then(response => response.json());
+	}
 };
