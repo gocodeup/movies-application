bbcf99779862a79206c77c6f9038039bf2168036
Christopher Caldwell <christophercaldwell@IT-33756.attlocal.net>
2019-07-24 17:26:21 -0500

    added a few more clarifying comments

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,14 @@
 const path = require("path");
 
 module.exports = {
-	entry: ["babel-polyfill", "./src/index.js"],
+	entry: ["babel-polyfill", "./src/index.js"], // change to ./src/promises.js to use that version
 	// needed to utilize async & await / promises
 	// where our app "starts"
 
 	// where to put the transpiled javascript
 	// this file is READ ONLY!!
+	// The block belows says, find the absolute path of the public folder, and stick a file called main.js in it.
+	// index.html is looking for a main.js file in the script tag
 	output: {
 		path: path.resolve(__dirname, "public"),
 		filename: "main.js"
@@ -28,6 +30,9 @@ module.exports = {
 		]
 	},
 	devServer: {
+		// the dev server is serving the public folder, which contains index.html.
+		// This is just like opening up html in the browser how we've normally done it, with the inclusipon of "hot reloading"
+		// Hot reloading refreshes and re-bundles the code each time changes are detected in the src/ directory.
 		contentBase: path.join(__dirname, "public"),
 		port: 1313,
 		compress: true,
