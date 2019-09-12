89068e58e9492137075b9055420bda3bb71bb4f8
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-12 09:23:31 -0500

    make movies populate in html

--- src/index.js
+++ src/index.js
@@ -9,13 +9,23 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
+const makeMovieCard = movie => {
+ let  html = `<p>${movie.title}</p>`;
+  html += `<p>${movie.rating}</p>`;
+  return html;
+};
+
 getMovies().then((movies) => {
-  $("#viewport").html("All done!");
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+  // $("#viewport").html("All done!");
+  // console.log('Here are all the movies:');
+  // movies.forEach(({title, rating, id}) => {
+    // console.log(`id#${id} - ${title} - rating: ${rating}`);
+  $("#viewport").html("");
+  movies.forEach((movie) => {
+    $("#viewport").append(makeMovieCard(movie));
+
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
