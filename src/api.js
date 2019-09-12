fcbf945978ff39894111a8fa02226f573a353715
Annette Iglesias <iglesiasannette@gmail.com>
2019-09-12 11:00:31 -0500

    completed function that displays movies

--- src/api.js
+++ src/api.js
@@ -6,3 +6,4 @@ module.exports = {
       .then(response => response.json());
   }
 };
+
--- src/index.js
+++ src/index.js
@@ -9,13 +9,33 @@ greeting('Ceres');
  * require style imports
  */
 const {getMovies} = require('./api.js');
+//OG
+// getMovies().then((movies) => {
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating, id}) => {
+//     console.log(`id#${id} - ${title} - rating: ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.')
+//   console.log(error);
+// });
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+
+// this function displays the movie selection and hides the spinner upon load
+  getMovies()
+      .then((movies) => {
+        // Always hide the spinner
+        document.querySelector('#spinner').style.display = 'none';
+        console.log('Here are all the movies:');
+
+        movies.forEach(({title, rating, id}) => {
+          return $('body').append(`id#${id} - ${title} - rating: ${rating}`);
+        })
+      })
+      .catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+      });
+
+
+
