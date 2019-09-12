edfa68a1263cf5849edbd1d321ef2fd3a40d6bbd
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-09-12 09:30:34 -0500

    populate movies as cards

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 const makeMovieCard = movie => {
- let  html = `<p>${movie.title}</p>`;
-  html += `<p>${movie.rating}</p>`;
+ // let  html = `<p>${movie.title}</p>`;
+ //  html += `<p>${movie.rating}</p>`;
+  let html = `<div>`;
+  html += `<div><h3>Rating: ${movie.rating} / 5</h3></div>`;
+  html += `<div><h2>${movie.title}</h2></div>`;
+  html += `</div>`;
   return html;
 };
 
 getMovies().then((movies) => {
-  // $("#viewport").html("All done!");
-  // console.log('Here are all the movies:');
-  // movies.forEach(({title, rating, id}) => {
-    // console.log(`id#${id} - ${title} - rating: ${rating}`);
   $("#viewport").html("");
   movies.forEach((movie) => {
     $("#viewport").append(makeMovieCard(movie));
