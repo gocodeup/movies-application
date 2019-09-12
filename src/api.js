c47e4768502903a55aa6f12f4dbc3b5aab6e705b
Matt Lopez <matthew.lopez4011@gmail.com>
2019-09-12 14:49:09 -0500

    Added functionality to add movies

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
+      // .then(data => console.log('GET was successful', data))
       .then(response => response.json());
   }
 };
--- src/index.js
+++ src/index.js
@@ -10,13 +10,78 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    $('.container').append(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
-});
+function refreshMovies(){
+  getMovies().then((movies) => {
+    $('#loading').html('');
 
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+      let movieItems = '';
+
+      movieItems += `<div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title"> ${title}</h4>
+                            <div class="card-text">
+                            Rating: ${rating}
+                                
+                            </div>
+                        </div>  
+                    </div>`;
+
+      $('.movies').append(movieItems);
+      console.log(title, rating, id);
+    });
+
+  }).catch((error) => {
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
+  });
+
+}
+
+refreshMovies(); //Initial call
+
+function newMovie(movieTitle, movieRating) {
+
+  const blogPost = {title: movieTitle, rating: movieRating};
+  const url = '/api/movies';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(blogPost),
+  };
+  fetch(url, options)
+      .then( (data) => console.log('Post was successful', data)/* post was created successfully */)
+      .catch( (data) => console.log('Post unsuccessful', data) /* handle errors */);
+
+
+  refreshMovies(); // Updates movies when submit is clicked
+  $('#movieTitle').val(''); // Clears out the input
+  $('#rating').val(''); // Clears out the drop down
+} // End of newMovie()
+
+
+// newMovie($('#movieTitle').val(), $('#rating').val() )
+
+
+
+  $('#btn').on('click',() => newMovie($('#movieTitle').val(), $('#rating').val()) );
+
+function modify(){
+  const blogPost = {title: 'Star Wars: A New Hope Directors Cut', rating: '4'};
+  const url = '/api/movies/1';
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(blogPost),
+  };
+  fetch(url, options)
+      .then( (data) => console.log('Post was successful', data)/* post was created successfully */)
+      .catch( (data) => console.log('Post unsuccessful', data) /* handle errors */);
+
+}
+modify();
