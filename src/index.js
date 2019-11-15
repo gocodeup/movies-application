e4b84188c8f22238533286673222751fda3d26d0
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-15 15:07:28 -0600

    able to append to the inner html

--- src/index.js
+++ src/index.js
@@ -6,11 +6,12 @@
  */
 const {getMovies} = require('./api.js');
 const $ = require("jquery");
+require('bootstrap');
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    document.body.innerHTML =`<h1>Id is ${id} number and the title is: ${title}, rated: ${rating}</h1>`;
+    $('#container').append(`<div>Id is ${id} number and the title is: ${title}, rated: ${rating}</div>`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
@@ -18,6 +19,7 @@ getMovies().then((movies) => {
 });
 
 
-$(window).load(function(){
-  $(".spinnertest").fadeOut("slow");
+$(window).on("load", function(){
+  $(".loader").fadeOut(2000);
 });
+
