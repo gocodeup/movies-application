d4e748731a45f54d5014fb984f29b916799d1dbe
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-11-15 12:33:03 -0600

    spinner working, loaded modules for bootstrap, going to lunch

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 /**
  * es6 modules and imports
  */
-import sayHello from './hello';
-sayHello('World');
-
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require("jquery");
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    document.body.innerHTML =`<h1>Id is ${id} number and the title is: ${title}, rated: ${rating}</h1>`;
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
+});
+
+
+$(window).load(function(){
+  $(".spinnertest").fadeOut("slow");
 });
