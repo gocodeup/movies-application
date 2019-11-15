88b1a0040597146805a301c350b61be0fa3b2769
Christopher Aguirre <christopher.aguirre210@gmail.com>
2019-11-15 16:40:39 -0600

    add funcationality done

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ const $ = require("jquery");
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    $('#container').append(`<div class="card"><div class="card-body">Id is ${id} number and the title is: ${title}, rated: ${rating}<br><span class="badge badge-primary">Edit Reel</span><span class="badge badge-danger">Trash Reel</span>
-</div></div>`);
+    $('#container').append(`<div class="card-img-top">pictures<div class="card"><div class="card-body">Id is ${id} number and the title is: ${title}, rated: ${rating}<br><span class="badge badge-primary">Edit Reel</span><span class="badge badge-danger">Trash Reel</span>
+</div></div></div>`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
 
@@ -23,7 +23,8 @@ $(window).on("load", function(){
   $(".loader").fadeOut(2000);
   $("#buttonadd").click(function(e){
     (e).preventDefault();
-      }
+    console.log($('#movietitle').val());
+    console.log($('#movierating').val());}
 
   )
 });
