403544296b002b745603a6fa53ade125f1339737
colinmarchbanks <marchbanks.colin@gmail.com>
2019-11-15 12:37:02 -0600

    loading message and added cards

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@
  * es6 modules and imports
  */
 import sayHello from './hello';
-sayHello('World');
+sayHello('Codeup');
 
 /**
  * require style imports
  */
 const {getMovies} = require('./api.js');
 
+const $ = require('jquery');
+
+
+
 getMovies().then((movies) => {
+  $('#loading').hide();
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    $('#content').append(`<div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+    </div>`)
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
