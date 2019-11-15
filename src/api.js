b8d7fde0ea9e21e7141527288143e3115cd7ea53
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-11-15 15:38:24 -0600

    Functionality on edit and delete button

--- src/api.js
+++ src/api.js
@@ -2,5 +2,14 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+
+  deleteMovie: (id) => {
+    return fetch(`/api/movies/${id}`,{
+      method: 'DELETE',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+    })
   }
 };
