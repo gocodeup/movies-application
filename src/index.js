22bf1464a66a17028724317d0ecdede88d24d56d
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-11-18 11:33:57 -0600

    edit modal opening and working w/ dynamically created cards

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@
 const {getMovies, postMovie} = require('./api.js');
 const $ = require("jquery");
 
+
+
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    $('#container').append(`<div class="card-img-top">pictures<div class="card"><div class="card-body">Id is ${id} number and the title is: ${title}, rated: ${rating}<br><span class="badge badge-primary">Edit Reel</span><span class="badge badge-danger">Trash Reel</span>
-</div></div></div>`);
+    $('#container').append(`<div class="card" id=${id}><div class="card-img-top">picture placeholder<div class="card-body">Id is ${id} number and the title is: ${title}, rated: ${rating}<br><button class="testbutton">Edit Reel</button></div></div></div>`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
@@ -30,7 +31,6 @@ $(window).on("load", function(){
   movie.rating = $("#movierating").val();
        postMovie(movie);
        getMovies().then((movies) => {
-         console.log('Here are all the movies:');
          $("#container").empty();
          movies.forEach(({title, rating, id}) => {
            $('#container').append(`<div class="card-img-top">pictures<div class="card"><div class="card-body">Id is ${id} number and the title is: ${title}, rated: ${rating}<br><span class="badge badge-primary">Edit Reel</span><span class="badge badge-danger">Trash Reel</span>
@@ -39,4 +39,3 @@ $(window).on("load", function(){
     })
   })
 });
-
