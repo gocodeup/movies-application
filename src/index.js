ffc19e899d76f933dd4c5af8a14c7bd181bb7876
Kenneth Howell <howell.kenneth.a@gmail.com>
2019-11-18 12:31:07 -0600

    grabbing title from individual divs complete

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const $ = require("jquery");
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    $('#container').append(`<div class="card" id=${id}><div class="card-img-top">picture placeholder<div class="card-body">Id is ${id} number and the title is: ${title}, rated: ${rating}<br><button class="testbutton">Edit Reel</button></div></div></div>`);
+    $('#container').append(`<div class="card" id=${id}><div class="card-img-top">picture placeholder<div class="card-body"><span id="${title}">Title: ${title}</span> rated: ${rating}<br><button class="testbutton">Edit Reel</button></div></div></div>`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
@@ -34,15 +34,19 @@ $(window).on("load", function(){
        getMovies().then((movies) => {
          $("#container").empty();
          movies.forEach(({title, rating, id}) => {
-           $('#container').append(`<div class="card-img-top">pictures<div class="card"><div class="card-body">Id is ${id} number and the title is: ${title}, rated: ${rating}<br><span class="badge badge-primary">Edit Reel</span><span class="badge badge-danger">Trash Reel</span>
+           $('#container').append(`<div class="card-img-top">pictures<div class="card"><div class="card-body">title: ${title}, rated: ${rating}<br><span class="badge badge-primary">Edit Reel</span><span class="badge badge-danger">Trash Reel</span>
 </div></div></div>`);
          })
     })
   });
 //  Updating the edit modal dynamically
 
-  $(".editmodal").click(function (e) {
-    
-  })
+  $(document).ready(function(){
+  $('body').on('click', '.testbutton', function() {
+      let buttonclicked = $(this);
+      let movietitle = buttonclicked.parent().find("span").attr("id");
+    console.log(movietitle);
+  });
+  });
 
 });
