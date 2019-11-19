b1cab5f356e954c572b5b0507766b5fca361f942
colinmarchbanks <marchbanks.colin@gmail.com>
2019-11-19 15:25:26 -0600

    finished project and presented

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-/**
- * es6 modules and imports
- */
 
-
-/**
- * require style imports
- */
 const $ = require('jquery');
 const {getMovie, getMovies, postMovie, patchMovie, deleteMovie} = require('./api.js');
 
-$("#addMovie").hide();
+const searchTerm = $("#searchTerm");
+const searchButton = $("#searchButton");
+const searchContainer = $("#searchContainer");
+let idEdited;
+
+
 
 const editClick = function (e) {
     $("#content").addClass("noDisplay");
     $("#addMovie").hide();
     $('#loading').show();
+    searchContainer.hide();
+
     let id = e.currentTarget.id;
     if (id.length === 7) {
         id.split('');
@@ -35,7 +35,8 @@ const editClick = function (e) {
     }
     let movieSelected = {};
     getMovie(id).then().then(movie => {
-        $("#loading").hide();
+        $('#loading').hide();
+        searchContainer.show();
         console.log("Making a request to edit a single movie");
         movieSelected = movie;
         $("#movieEditTitle").val(movieSelected.title);
@@ -52,7 +53,7 @@ const createMovieContent = () => {
 
         $('#content').html("");
         getMovies().then((movies) => {
-            if(movies[0] === undefined){
+            if (movies[0] === undefined) {
                 $("#addMovie").show();
             }
             $('#loading').hide();
@@ -70,11 +71,8 @@ const createMovieContent = () => {
                         releaseDate = `${response.results[0].release_date}`;
                         summary = `${response.results[0].overview}`;
                         actualTitle = `${response.results[0].title}`;
-                        actualRating= `${response.results[0].vote_average}`;
-                        console.log(releaseDate);
-                        console.log(summary);
-                        // `http://image.tmdb.org/t/p/w500/${json.results[0].poster_path}`
-                $('#content').append(`<div class="card m-2 cardBackground d-flex" style="width: 18rem">
+                        actualRating = `${response.results[0].vote_average}`;
+                        $('#content').append(`<div class="card m-2 cardBackground d-flex" style="width: 18rem">
             <img src="${result}" class="card-img-top" alt="...">
             <div class="card-body roboto">
             <h5 class="card-text text-center">${actualTitle}</h5>
@@ -88,81 +86,25 @@ const createMovieContent = () => {
             </div>
         </div>
         </div>`)
-            $('.deleteButton').off().on('click', deleteClick);
-            $('.editButton').off().on('click', editClick);
-            $("#addMovie").show();
-                    })});
+                        $('.deleteButton').off().on('click', deleteClick);
+                        $('.editButton').off().on('click', editClick);
+                        $("#addMovie").show();
+                        searchContainer.show();
+
+                    })
+            });
         }).catch((error) => {
             alert('Oh no! Something went wrong.\nCheck the console for details.');
             console.log(error);
         });
     }));
 };
-//get all movies
-createMovieContent();
-
-
-
-
-$("#addMovieButton").click(function () {
-    $("#content").html("");
-    $("#loading").show();
-    $("#addMovie").hide();
-    if($('#movieTitleInput').val().trim() === ""){
-        createMovieContent().then(() => {
-            $("#loading").hide();})
-    }else{
-    postMovie({
-        "title": $('#movieTitleInput').val(),
-        "rating": $("#ratingSelect").val(),
-    }).then(getMovies).then(() => {
-        createMovieContent().then(() => {
-            $("#loading").hide();}
-        );});}});
-
-let idEdited;
-
-
-
-$("#editSave").click(function () {
-    $("#loading").show();
-    $(".modal").hide();
-    $('#content').html("");
-    $("#content").removeClass("noDisplay");
-    let editedMovie = {
-        "title": $("#movieEditTitle").val(),
-        "rating": $("#ratingEdit").val(),
-        "id": idEdited
-    };
-    if(editedMovie.title.trim() === ""){
-        createMovieContent().then(() => {
-            $("#loading").hide();
-        });
-    }
-    else{
-        patchMovie(editedMovie, idEdited).then(() => {
-            createMovieContent().then(() => {
-                $("#loading").hide();
-            });
-        });
-    }
-});
-
-$(".close").click(function () {
-    $(".modal").hide();
-    $("#loading").show();
-    $('#content').html("");
-    $("#content").removeClass("noDisplay");
-    createMovieContent().then(() => {
-        $("#loading").hide();
-    });
-
-});
 
 const deleteClick = function (e) {
     $("#content").addClass("noDisplay");
     $("#addMovie").hide();
     $('#loading').show();
+    searchContainer.hide();
     let id = e.currentTarget.id;
     if (id.length === 12) {
         id.split('');
@@ -173,7 +115,6 @@ const deleteClick = function (e) {
         bucket.push(id[11]);
         bucket.push(id[12]);
         id = bucket.join('');
-        console.log("option 2 :" + id);
     } else if (id.length === 14) {
         let bucket = [];
         id.split('');
@@ -182,14 +123,139 @@ const deleteClick = function (e) {
         bucket.push(id[13]);
         id = bucket.join('');
     }
-    console.log(id);
     deleteMovie(id).then(() => {
         $('#content').html("");
         $("#content").removeClass("noDisplay");
         createMovieContent().then(() => {
-            $("#loading").hide();
+            $('#loading').hide();
+            searchContainer.show();
         });
     })
 };
+
+$("#addMovie").hide();
+
+searchContainer.hide();
+
+createMovieContent();
+
+$("#addMovieButton").click(function () {
+    $("#content").html("");
+    $('#loading').show();
+    searchContainer.hide();
+    $("#addMovie").hide();
+    if ($('#movieTitleInput').val().trim() === "") {
+        createMovieContent().then(() => {
+            $('#loading').hide();
+            searchContainer.hide();
+        })
+    } else {
+        postMovie({
+            "title": $('#movieTitleInput').val(),
+            "rating": $("#ratingSelect").val(),
+        }).then(getMovies).then(() => {
+            createMovieContent().then(() => {
+                    $('#loading').hide();
+                    searchContainer.show();
+                }
+            );
+        });
+    }
+});
+
+$("#editSave").click(function () {
+    $('#loading').show();
+    searchContainer.hide();
+    $(".modal").hide();
+    $('#content').html("");
+    $("#content").removeClass("noDisplay");
+    let editedMovie = {
+        "title": $("#movieEditTitle").val(),
+        "rating": $("#ratingEdit").val(),
+        "id": idEdited
+    };
+    if (editedMovie.title.trim() === "") {
+        createMovieContent().then(() => {
+            $('#loading').hide();
+            searchContainer.show();
+        });
+    } else {
+        patchMovie(editedMovie, idEdited).then(() => {
+            createMovieContent().then(() => {
+                $('#loading').hide();
+                searchContainer.show();
+            });
+        });
+    }
+});
+
+$(".close").click(function () {
+    $(".modal").hide();
+    $('#loading').show();
+    searchContainer.hide();
+    $('#content').html("");
+    $("#content").removeClass("noDisplay");
+    createMovieContent().then(() => {
+        $('#loading').hide();
+        searchContainer.show();
+    });
+
+});
+
+searchButton.on("click", function () {
+    $("#content").addClass("noDisplay");
+    $("#addMovie").hide();
+    $('#loading').show();
+    searchContainer.hide();
+    getMovies().then((movies) => {
+        $('#content').html("");
+        if (movies[0] === undefined) {
+            $("#addMovie").show();
+        }
+        $('#loading').hide();
+        searchContainer.show();
+        console.log('Here are all the movies:');
+        movies.forEach(({title, rating, id}) => {
+            let result;
+            let releaseDate;
+            let summary;
+            let actualTitle;
+            let actualRating;
+            console.log({title, rating, id});
+            if (title.toLowerCase().includes(searchTerm.val().toLowerCase().trim())) {
+                $.getJSON("https://api.themoviedb.org/3/search/movie?api_key=15d2ea6d0dc1d476efbca3eba2b9bbfb&query=" + title + "&callback=?")
+                    .then(response => {
+                        result = `http://image.tmdb.org/t/p/w500/${response.results[0].poster_path}`;
+                        releaseDate = `${response.results[0].release_date}`;
+                        summary = `${response.results[0].overview}`;
+                        actualTitle = `${response.results[0].title}`;
+                        actualRating = `${response.results[0].vote_average}`;
+                        $('#content').append(`<div class="card m-2 cardBackground d-flex" style="width: 18rem">
+            <img src="${result}" class="card-img-top" alt="...">
+            <div class="card-body roboto">
+            <h5 class="card-text text-center">${actualTitle}</h5>
+            <p class="card-text text-center mt-4">Score: ${actualRating}</p>
+            <p class="card-text text-center">Released: ${releaseDate}</p>
+            <p class="card-text text-center">${summary}</p>
+            <p class="card-text text-center mb-5">Personal Rating: ${rating}</p>
+            <div class="" id="buttonGroup">
+                <button class = "btn btn-dark editButton text-center mr-3" id="button${id}">Edit Movie</button>
+                <button class = "btn btn-dark deleteButton text-center ml-3" id="deleteMovie${id}">Delete</button>
+            </div>
+        </div>
+        </div>`);
+                        $('.deleteButton').off().on('click', deleteClick);
+                        $('.editButton').off().on('click', editClick);
+                        $("#addMovie").show();
+                        searchContainer.show();
+                        $("#content").removeClass("noDisplay");
+                    })
+            }
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
+});
 
 
