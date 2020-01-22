61aedbb80f0dc3ab989fc0c952c9a3fd0b1a8173
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-22 17:02:34 -0600

    Added loading js stuff.

--- src/index.js
+++ src/index.js
@@ -20,3 +20,14 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
+
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+function removeLoader(){
+  $( "#loadingDiv" ).fadeOut(500, function() {
+    // fadeOut complete. Remove the loading div
+    $( "#loadingDiv" ).remove(); //makes page more lightweight
+  });
+}
