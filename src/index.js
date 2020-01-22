0b9b3ecb1e16cd8dd201fa73a11733f1dde6b9b8
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-01-22 16:37:12 -0600

    handing over

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 /**
  * es6 modules and imports
  */
+
+const $ = require('jquery');
+
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
@@ -10,11 +14,34 @@ sayHello('World');
 const {getMovies} = require('./api.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+    console.log('Here are all the movies:');
+    movies.forEach(({title, rating, id}) => {
+        console.log(`id#${id} - ${title} - rating: ${rating}`);
+    });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
 });
+
+// loader------------------------------
+$('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
+$(window).on('load', function () {
+    setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+});
+
+function removeLoader() {
+    $("#loadingDiv").fadeOut(500, function () {
+        // fadeOut complete. Remove the loading div
+        $("#loadingDiv").remove(); //makes page more lightweight
+    });
+}
+
+//pulling movie data----------------------
+
+
+// $.ajax("db.json").done(function(data){
+//   $('#submit').click(function(){
+//     $('#movies').html(`<h1> hello</h1>`)
+//   });
+// }
+
