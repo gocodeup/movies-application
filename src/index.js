3172419ae6618f0e03b83efd18f20bfbde840aae
Crystal Thibodeau <crystalthibodeau2@gmail.com>
2020-01-23 15:35:53 -0600

    function one of three complete

--- src/index.js
+++ src/index.js
@@ -1,42 +1,101 @@
 /**
  * es6 modules and imports
  */
-
 const $ = require('jquery');
 
 import sayHello from './hello';
-
 sayHello('World');
 
-/**
- * require style imports
- */
-const {getMovies, addMovies, getMovie, editMovie, deleteMovie} = require('./api.js');
+const {getMovies} = require('./api.js');
 
-let $movies = $('#movies');
 
-getMovies().then((movies) => {
-    // console.log('Here are all the movies:');
-    movies.forEach(({title, rating, id}) => {
-        $movies.append(`<li class="list-group-item"> id#${id} - ${title} - rating: ${rating}</li>`);
-        // console.log(`id#${id} - ${title} - rating: ${rating}`);
-    });
+
+function showMovies(){
+    getMovies().then((movies) => {
+        $('.movies').html('');
+        movies.forEach(({title, rating, id}) => {
+            let movieItems = '';
+                movieItems += `
+
+
+                <div class="mt-5 pt-5">
+                        <div class="mt-5 pt-5">
+                            <h4> ${title}</h4>
+                            <div>
+                                <div>
+                                Rating: ${rating}
+                                </div>
+                                <!-- Button trigger modal -->
+                                <button type="button">
+                                  Edit
+                                </button>
+                            </div>
+                            <!-- Modal -->
+                            <div>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <h5>Edit Movie</h5>
+                                             <button type="button">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div>
+                                            <p>Edit Movie Title</p>
+                                        <!--input-->
+                                            <input id="editTitle" type="text" value="" placeholder="Movie Title">
+                                            <p>Edit Movie Raiting</p>
+                                        <!--Select-->
+                                            <div>
+                                                <select>
+                                                    <option value="0" selected>Choose...</option>
+                                                    <option value="1">1</option>
+                                                    <option value="2">2</option>
+                                                    <option value="3">3</option>
+                                                    <option value="4">4</option>
+                                                    <option value="5">5</option>
+                                                </select>
+                                            </div>
+                                            </div>
+                                        <div>
+                                            <button type="button" class="btn btn-primary mr-auto btn-size" id="deleteMovieBtn">Delete Movie</button>
+                                            <button type="button" class="btn btn-primary btn-size" data-dismiss="modal">Close</button>
+                                            <button type="button" id="saveEditBtn" class="btn btn-primary btn-size" data-dismiss="modal">Save changes</button>
+                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                `;
+
+
+            $('.movies').append(movieItems);
+        });
 }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
+    alert('There was an error loading the page..');
 });
+}
 
 
-$('#movieSearch').keyup(function() {
-    let movieName = $("#movieSearch").val();
-    console.log(movieName);
-    // let found = movies.find(function (element){
-    //     return element;
-    // })
-});
+// function refreshMovies() {
+//     getMovies().then((movies) => {
+//         // $('#loading').html('').css('display', 'none');
+//         $('.movies').html('');
+//         // Show add movie inputs on load
+//         // $('.addMovieInputs').css('display', 'block').css('text-align', 'center');
+//         movies.forEach(({title, rating, id}) => {
+//             let movieItems = '';
+//             movieItems += ` Rating: ${rating} Title: ${title} Id:${id}`;
+//             $('.movies').append(movieItems);
+//         });
+//     }) // End of GetMovies()
+//         .catch((error) => {
+//         });
+// } // End of refreshMovies
 
-// let userSearch = $("#movieSearch").val();
-
+showMovies();
 
 // loader------------------------------
 $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
@@ -50,10 +109,3 @@ function removeLoader() {
         $("#loadingDiv").remove(); //makes page more lightweight
     });
 }
-
-
-// // $.ajax("db.json").done(function(data){
-// // $('#submit').click(function(){
-// //   $('#movies').html(`<h1> hello</h1>`)
-// // });
-// // }
