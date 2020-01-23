67550a871c954855ef2b31fb1c6bfda7f2830cac
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-01-22 19:04:50 -0600

    added list items

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-/**
- * es6 modules and imports
- */
-import sayHello from './hello';
-sayHello('World');
+$(document).ajaxStart(function(){
+  $("#wait").css("display", "block");
+});
+$(document).ajaxComplete(function(){
+  $("#wait").css("display", "none");
+});
 
-/**
- * require style imports
- */
+
+
 const {getMovies} = require('./api.js');
+let $movies = $('#movies');
+
+// show a listing of the movies that are in the database
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  // alert('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
+    $movies.append(`<li> id#${id} - ${title} - rating: ${rating}</li>`);
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong. Check the console for details.')
   console.log(error);
 });
+
+
+
+
 
