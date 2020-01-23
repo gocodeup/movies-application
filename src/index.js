06f01f2b90e6bd63af0fdac2d446b3439e4f1a9e
Gaston Tarango <gaston.tarango@gmail.com>
2020-01-23 10:23:54 -0600

    did this work?

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const $ = require('jquery');
 /**
  * es6 modules and imports
  */
@@ -13,16 +12,23 @@ const {getMovies} = require('./api.js');
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    console.log(`id#${id} - ${title} - rating: ${rating} - ${genre}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.Check the console for details.');
+  alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
- //functionality for the loading pane//
-let loader1 = $('.container').hide().show(8000).hide(8000);
-let loader2 = $('.center').hide().show(8000).hide(8000);
 
-
-
-
+function refresh() {
+  let inventory = $.get("data/inventory.json");
+  inventory.done(function (data) {
+    $.each(data, function (index, item) {
+      $('#insertProducts').append('<tr>' + '<td>' + item.title + '</td>' + '<td>' + item.quantity + '</td>' + '<td>' + item.price + '</td>' + '<td>' + item.categories + '</td>' + '</tr>');
+    })
+  })
+}
+refresh();
+$('#refresh').click(function () {
+  $('#insertProducts').html("");
+  refresh()
+})
