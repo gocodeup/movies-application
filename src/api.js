f222e5a874c22de39f4265d690b526cf15f4933e
Noland Seigler <nolandseigler@gmail.com>
2020-01-23 14:03:24 -0600

    updated OMDB to search multi will be used for pictures

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
-const OMDB_API_KEY = require('./keys.js');
+const {OMDB_API_KEY} = require('./keys.js');
+const {THE_MOVIE_DB_API_KEY} = require('./keys.js');
 module.exports = {
     getMovies: () => {
         return fetch('/api/movies')
@@ -21,9 +22,58 @@ module.exports = {
         } else if (inputMovieTitle.split(" ").length === 1) {
             searchParams = inputMovieTitle;
         }
-       return fetch(`http://www.omdbapi.com/?t=${searchParams}&apikey=${OMDB_API_KEY}`)
+        http://www.omdbapi.com/?apikey=[yourkey]&
+       return fetch(`http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${searchParams}`)
            .then((response) => response.json());
-   }
+   },
+    searchTheMovieDBID: (inputMovieTitle) => {
+        // https://api.themoviedb.org/3/search/movie?api_key={key}&language=en-US&query=Star%20Wars&page=1&include_adult=false
+        let searchParams = '';
+        inputMovieTitle = inputMovieTitle.toLowerCase();
+        if (inputMovieTitle.split(" ").length > 1) {
+            let titleArr = inputMovieTitle.split(" ");
+            let finalStr = '';
+            let outputArr = [];
+            titleArr.forEach(element => {
+                outputArr.push(element + '%20');
+            });
+            let outputStr = outputArr.join('');
+            finalStr = outputStr.substring(0, outputStr.length - 3);
+            console.log(finalStr);
+            searchParams = finalStr;
+        } else if (inputMovieTitle.split(" ").length === 1) {
+            searchParams = inputMovieTitle;
+        }
+        // https://api.themoviedb.org/3/search/movie?api_key={key}&language=en-US&query=Star%20Wars&page=1&include_adult=false
+        return fetch(`https://api.themoviedb.org/3/search/movie?api_key=${THE_MOVIE_DB_API_KEY}&language=en-US&query=${searchParams}&page=1&include_adult=false`)
+            .then((response) => response.json());
+    },
+    searchMovie: (inputObj) => {
+        let movieID = inputObj['id'];
+        https://api.themoviedb.org/3/movie/${movieID}?api_key=${THE_MOVIE_DB_API_KEY}&language=en-US
+        return fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${THE_MOVIE_DB_API_KEY}&language=en-US`)
+            .then((response) => response.json());
+    },
+    updateCRUDyDB: (inputObj) => {
+        // let data = {
+        //     title: inputObj['title'],
+        //     overview: inputObj['overview'],
+        //     genre: inputObj["genres"][0]["name"]
+        // };
+        let data = {username: "user"}
+        return fetch(`/api/movies`,  {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+            // .then(response => response.json())
+    }
 };
 
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 //modules required
 const $ = require('jquery');
-const OMDB_API_KEY = require('./keys.js');
+const {OMDB_API_KEY} = require('./keys.js');
+const {THE_MOVIE_DB_API_KEY} = require('./keys.js');
 const {getMovies} = require('./api.js');
 const {getOMDB} = require('./api.js');
+const {searchTheMovieDBID} = require('./api.js');
+const {searchMovie} = require('./api.js');
+const {updateCRUDyDB} = require('./api.js');
 // const {movieData} = require('./api.js');
 getMovies()
     .then((movies) => {
@@ -21,8 +25,20 @@ getMovies()
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
 });
-getOMDB("Bad Boys")
+getOMDB("Ninja Turtles")
     .then(response => console.log(response));
+// searchTheMovieDBID("Star Wars The Phantom Menace")
+//     // .then(response => console.log(response))
+//     .then(response => {
+//         return response['results'][0];
+//     })
+//     .then(response => searchMovie(response))
+//     // .then(response => console.log(response))
+//     .then(response => updateCRUDyDB(response))
+updateCRUDyDB('cat');
+
+
+
 
 
 
