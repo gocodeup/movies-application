36d892e962bd1c6e7ff9af2e8c07768b59077d18
johns-santos <johnsantos@Johns-MacBook-Pro.local>
2020-01-23 13:53:12 -0600

    DELETE and CREATE add to api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,37 @@
+
 module.exports = {
+
   getMovies: () => {
     return fetch('/api/movies')
-      .then(response => response.json());
+        .then(response => response.json());
   },
-};
 
+  // deleteMovie: ({id}) => {
+  //   let myHeaders = new Headers();
+  //   myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  //   let movieID = {id};
+  //
+  //   return fetch('/api/movies/', {
+  //     method: `DELETE`,
+  //     body: new URLSearchParams(movieID),
+  //     redirect: 'follow'
+  //   })
+  // },
+
+  deleteMovie: ({id}) => {
+    let url = '/api/movies/';
+    return fetch(url + '/' + id, {
+      method: 'DELETE'
+    })
+        .then(response => response.json());
+  },
+
+  createMovie: ({title, rating}) => {
+    let newMovie = { title, rating };
+    return fetch('/api/movies/', {
+      method: 'POST',
+      body: JSON.stringify((newMovie))
+    })
+  },
+
+};
--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-
-
-
 setTimeout(function(){loader.style.display = 'none';}, 1200);
 
+
+
 const {getMovies} = require('./api.js');
+const {deleteMovie} = require('./api.js');
 let $movies = $('#movies');
 
 
 // show a listing of the movies that are in the database
-
 getMovies().then((movies) => {
   // alert('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
-    $movies.append(`<li class="list-group-item"> id#${id}  ${title}  rating: ${rating}<button class="btn btn-outline-danger btn-sm float-right delete">X</button></li>`);
+    $movies.append(`<li class="list-group-item"> id#${id}  ${title}  rating: ${rating}<button id="deleteMovie" class="btn btn-outline-danger btn-sm float-right delete">X</button></li>`);
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
 }).catch((error) => {
@@ -22,19 +21,18 @@ getMovies().then((movies) => {
 
 
 
+
 // a way to post movies
-
-
-const url = '/posts';
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(blogPost),
-};
-fetch(url, options)
-    .then(/* post was created successfully */)
-    .catch(/* handle errors */);
+// const url = '/posts';
+// const options = {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(blogPost),
+// };
+// fetch(url, options)
+//     .then(/* post was created successfully */)
+//     .catch(/* handle errors */);
 
 
