bd4e3a070782e02694a8494771592560b9de0961
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-23 07:52:30 -0600

    submitting title and rating works

--- src/api.js
+++ src/api.js
@@ -1,14 +1,15 @@
 // export const getMovies = () => fetch('/api/movies')
 //       .then(response => response.json());
 
-const moviePost = {title: 'The Sandlot', rating: '4', id: 4};
+// const movie = {title: 'The Matrix 4', rating: '3', id: 9};
 const url = '/api/movies';
-const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(moviePost),
-};
-export const getMovies = () => fetch(url, options)
-    .then(response => response.json());
+// const options = {
+//     method: 'get',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     // body: JSON.stringify(moviePost2),
+//     // body: JSON.stringify(movie),
+//     };
+export const getMovies = () => fetch(url)
+    .then(response => response.json())
--- src/index.js
+++ src/index.js
@@ -1,23 +1,76 @@
+import $ from 'jquery'
 /**
  * es6 modules and imports
  */
 import sayHello from './hello';
+
 sayHello('World');
 
 /**
  * require style imports
  */
-import {getMovies} from './api.js';
+import {getMovies} from './api';
 
 // import loadingGifMain from './hello';
 
+const generateTable = () => {
+    getMovies().then((movies) => {
+        // Add table to container
+        let movieTable = '<table class="movies highlight centered" id="movie-list">';
+        movieTable += '<thead>';
+        movieTable += '<tr>';
+        movieTable += '<th>Movie</th>';
+        movieTable += '<th>Rating</th>';
+        movieTable += '<th>Options</th>';
+        movieTable += '</tr>';
+        movieTable += '</thead>';
+        // console.log('Here are all the movies:');
+        movies.forEach(({title, rating, id}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+            //renders movie and rating in table rows
+            movieTable += `<tr><td id="row-title">${title}</td><td id="row-rating">${rating}</td>`;
+            movieTable += `<td id="row-button">` + `</td></tr>`
+            // add table edit and delete
+            ;
+        });
+        //closes table after content is rendered
+        movieTable += '</table>';
+        // adds fetched table info to HTML
+        $('.container').html(movieTable)
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    })
+        .catch((error) => {
+            alert('Oh no! Something went wrong.\nCheck the console for details.')
+            console.log(error);
+        })
+};
+generateTable();
+
+//Submit form
+$('#submit-button').on("click", function (e) {
+    e.preventDefault();
+
+    if ($('#movie-title-input').val() !== "" && $('#movie-rating-input option:selected').val() !== "") {
+
+        let title = $('#movie-title-input').val();
+        let rating = $('#movie-rating-input').val();
+        let movie = {
+            title: title,
+            rating: rating,
+        };
+
+        const url = '/api/movies';
+        const options = {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({title, rating}),
+        };
+        fetch(url, options).then((response) => {
+            response.json();
+        }).then(() => {
+            generateTable();
+        });
+    }
 });
