4b963bd77bb75e8df0465f85f76bdad8da332bce
Miguel Vera <miguel.vera7908@gmail.com>
2020-01-24 08:38:58 -0600

    Worked on edit functionality

--- src/index.js
+++ src/index.js
@@ -12,31 +12,40 @@ const $ = require('jquery');
 const {getMovies, addMovie, getMovie, editMovie, deleteMovie} = require('./api.js');
 
 function renderMovies() {
-  $('.movies').html('');
-  getMovies().then((movies) => {
     $('.movies').html('');
-    movies.forEach(({title, rating, id, genre}) => {
-      $('.movies').append(`<div class="card h-100"><div class="card-body"><p>Id: ${id}</p><p>Title: ${title}</p><p>Rating: ${rating}</p><p>Genre: ${genre}</p><button data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapseExample" id="editButton" class="btn btn-primary edit" value="${id}">Edit</button><button id="deleteButton" class="btn btn-danger delete" value="${id}">Delete</button></div></div>`)
-    });
-  })
+    getMovies().then((movies) => {
+        $('.movies').html('');
+        movies.forEach(({title, rating, id, genre}) => {
+            $('.movies').append(`
+                <div class="card h-100">
+                <div class="card-body"><p>Id: ${id}</p>
+                <p>Title: ${title}</p>
+                <p>Rating: ${rating}</p>
+                <p>Genre: ${genre}</p>
+                <button id="editButton" class="btn btn-primary edit" data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapseExample"value="${id}">Edit</button>
+                <button id="deleteButton" class="btn btn-danger delete" value="${id}">Delete</button>
+                </div>
+                </div>`)
+        });
+    })
 }
 
 getMovies().then((movies) => {
-  $('.movie-database').html('Here are all the movies:');
-  movies.forEach(({title, rating, id, genre}) => {
-    $('.movies').append(`<div class="card h-100"><div class="card-body"><p>Id: ${id}</p><p>Title: ${title}</p><p>Rating: ${rating}</p><p>Genre: ${genre}</p><button data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapseExample" id="editButton" class="btn btn-primary edit" value="${id}">Edit</button><button id="deleteButton" class="btn btn-danger delete" value="${id}">Delete</button></div></div>`)
-  });
+    $('.movie-database').html('Here are all the movies:');
+    movies.forEach(({title, rating, id, genre}) => {
+        $('.movies').append(`<div class="card h-100"><div class="card-body"><p>Id: ${id}</p><p>Title: ${title}</p><p>Rating: ${rating}</p><p>Genre: ${genre}</p><button data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapseExample" id="editButton" class="btn btn-primary edit" value="${id}">Edit</button><button id="deleteButton" class="btn btn-danger delete" value="${id}">Delete</button></div></div>`)
+    });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
-  console.log(error);
+    alert('Oh no! Something went wrong.\nCheck the console for details.');
+    console.log(error);
 });
 
 $('#addMovie').click(() => {
-  let title = $('#inputTitle').val();
-  let rating = $('#inputRating').val();
-  let genre = $('#inputGenre').val();
-  // look up inputs
-  addMovie(title, rating, genre);
+    let title = $('#inputTitle').val();
+    let rating = $('#inputRating').val();
+    let genre = $('#inputGenre').val();
+    // look up inputs
+    addMovie(title, rating, genre);
 });
 
 // $('#deleteMovie').click(() => {
@@ -45,27 +54,27 @@ $('#addMovie').click(() => {
 //   renderMovies();
 // });
 
-$('.movies').on('click', '.edit', function (event){
-  let editId = $(event.target).val();
-  editMovie(editId);
-  renderMovies();
+$('.movies').on('click', '.delete', function (event) {
+    let deleteId = $(event.target).val();
+    deleteMovie(deleteId);
+    renderMovies();
 });
 
-$('#editMovie').on('click', function (e) {
-  e.preventDefault();
-  $("#editForm").toggle();
-  let movieName = $('#editTitle').val();
-  let rating = $('#editRating').val();
-  let newGenre = $('#editGenre').val();
-  // $('#editTitle').val('');
-  // $('#editRating').val('');
-  // $('#editGenre').val('');
-  let movieData = {title: movieName, rating: rating, genre: newGenre};
-  let editId = id;
-  console.log(movieData);
-  editMovie(editId, movieData);
-      // .then(console.log('It worked')).catch(console.log('Did not work'));
-  renderMovies();
+$('#editMovie').on('click', '.edit', function (e) {
+    e.preventDefault();
+    $("#editForm").toggle();
+    let movieName = $('#editTitle').val();
+    let rating = $('#editRating').val();
+    let newGenre = $('#editGenre').val();
+    // $('#editTitle').val('');
+    // $('#editRating').val('');
+    // $('#editGenre').val('');
+    let movieData = {title: movieName, rating: rating, genre: newGenre};
+    let editId = id;
+    console.log(movieData);
+    editMovie(editId, movieData);
+    // .then(console.log('It worked')).catch(console.log('Did not work'));
+    renderMovies();
 });
 
 
@@ -81,15 +90,15 @@ $('#editMovie').on('click', function (e) {
 // });
 
 
-
 //Function for loading screen
 $('body').append('<div style="" id="loadingDiv"><div class="loader">Loading...</div></div>');
-$(window).on('load', function(){
-  setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
+$(window).on('load', function () {
+    setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
 });
-function removeLoader(){
-  $( "#loadingDiv" ).fadeOut(500, function() {
-    // fadeOut complete. Remove the loading div
-    $( "#loadingDiv" ).remove(); //makes page more lightweight
-  });
+
+function removeLoader() {
+    $("#loadingDiv").fadeOut(500, function () {
+        // fadeOut complete. Remove the loading div
+        $("#loadingDiv").remove(); //makes page more lightweight
+    });
 }
