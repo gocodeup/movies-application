9461ba5efc82658e6517ea808b0528cc3a253af7
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-01-24 11:13:26 -0600

    Added a form to the cards. Added an edit jquery function.

--- src/index.js
+++ src/index.js
@@ -22,8 +22,27 @@ function renderMovies() {
                 <p>Title: ${title}</p>
                 <p>Rating: ${rating}</p>
                 <p>Genre: ${genre}</p>
-                <button id="editButton" class="btn btn-primary edit" data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapseExample"value="${id}">Edit</button>
+                <button class="btn btn-primary edit editButton" data-toggle="collapse" data-target="#collapse" value="${id}">Edit</button>
                 <button id="deleteButton" class="btn btn-danger delete" value="${id}">Delete</button>
+                
+                <div class="collapse" id="collapse" aria-expanded="false" aria-controls="collapse">
+                    <form action="" class="editForm">
+                        <div class="form-group">
+                            <label for="inputTitle">Title</label>
+                            <input type="text" class="form-control editTitle">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputRating">Rating</label>
+                            <input type="text" class="form-control editRating">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputGenre">Genre</label>
+                            <input type="text" class="form-control editGenre">
+                        </div>
+                        <button class="editMovie">Edit Movie</button>
+                    </form>
+                </div>
+                
                 </div>
                 </div>`)
         });
@@ -60,16 +79,23 @@ $('.movies').on('click', '.delete', function (event) {
     renderMovies();
 });
 
-$('#editMovie').on('click', '.edit', function (e) {
+// this edit is for the cards
+$('.editButton').on('click', function (event){
+  let editId = $(event.target).val();
+  $('.editForm').toggle(editId);
+});
+
+// this edit is for the form
+$('.editMovie').on('click', '.edit', function (e) {
     e.preventDefault();
     $("#editForm").toggle();
-    let movieName = $('#editTitle').val();
-    let rating = $('#editRating').val();
+    let newMovieName = $('#editTitle').val();
+    let newRating = $('#editRating').val();
     let newGenre = $('#editGenre').val();
     // $('#editTitle').val('');
     // $('#editRating').val('');
     // $('#editGenre').val('');
-    let movieData = {title: movieName, rating: rating, genre: newGenre};
+    let movieData = {title: newMovieName, rating: newRating, genre: newGenre};
     let editId = id;
     console.log(movieData);
     editMovie(editId, movieData);
