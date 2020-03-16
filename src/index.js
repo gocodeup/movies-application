96c78541f011c0517da3e840582f757a6b6886d6
James Muralles <james.muralles23@gmail.com>
2020-03-15 22:39:31 -0500

    fixed delete function

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
 // function to add a loading animation
     $(window).on('load', function () {
-        $('.movieList').css("display", "none");
+        $('.movieList').css("display", "none"); //while loading no list is displayed
         setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
     });
 
@@ -22,12 +22,12 @@ $(document).ready(function () {
     function removeLoader() {
         $(".overlay-loader").fadeOut(500, function () {
             // fadeOut complete. Remove the loading div
-            $("#overlay-loader").remove();
-            $('.movieList').css("display", "inline-block")
+            $("#overlay-loader").remove(); //removes loader after set time
+            $('.movieList').css("display", "inline-block") //displays movie list again
         });
     }
 
-//this function will console log the movie that you got from the API
+//this function will console log the movie that you got from the API and add it to the #insertMovie
     function movieUpdate() {
         getMovies().then((movies) => {
             console.log('Here are all the movies:');
@@ -45,25 +45,34 @@ $(document).ready(function () {
         });
     }
 
-    movieUpdate();
+    function movieDelete() {
+        $('#delete-movie').click(function () { //when you click gets movies from json file
+            getMovies().then((movies) => {
+                movies.forEach(({title, rating, id}) => { //loops through each movie in the array
+                    console.log(movies);
+                    if (title === $('#input-movie').val()) { // if the title you input is equal to the title of json object
+                        // console.log(movies.title);
+                        console.log("movie deleted is: " + title, id);
+                        $('#insertMovies').html(""); // clear list
+                        deleteMovie(id).then(movieUpdate); //movie id is deleted and list is updated
+                    } else {
+                        console.log("not a movie")
+                    }
+                });
 
-    function movieUpdateDelete() {
-        getMovies().then((movies) => {
-            console.log('Here are all the movies:');
-            movies.forEach(({title, rating, id}) => { //loops through each movie in the array
-                $('#insertMovies').html(`<tr>
-                                        <th>${title}</th>
-                                        <th>${rating}</th>
-                                        </tr>`);
-                console.log(`id#${id} - ${title} - rating: ${rating}`);//logs movie
-            });
-//catch in case something goes wrong
-        }).catch((error) => {
-            alert('Oh no! Something went wrong.\nCheck the console for details.');
-            // console.log(error);
+                $('#input-movie').val(''); //clears search box
+
+            })// after item is deleted this will update the hmtl with current movies
+
+
         });
+
+
     }
 
+
+    movieUpdate();
+    movieDelete();
 
 //will add a new movie to the jb.son file and log the new movies array. Will later have it added to a library appended to the body.
     $('#add-movie').click(function () {//on click will add the new movie to the json file
@@ -71,84 +80,19 @@ $(document).ready(function () {
             "title": $('#input-movie').val(),
             "rating": $('#input-rating').val()
         })
-        $('#input-movie').val('');
-        $('#input-rating').val('');
+        $('#input-movie').val(''); //clears input
+        $('#input-rating').val(''); //clears input
+        $('#insertMovies').html("");// clears list
+        movieUpdate(); //updates list with no added movie
     });
 
-    $("#add-movie").click(function () { //on click will add movie to html
-        $('#insertMovies').html(null);
-        movieUpdate();
 
-    });
-    // function to add a loading animation
+    //removes  movie list on load to show loading animation
     $(".movieList").on('load', function () {
         $('.movieList').css("display", "none");
         setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
     });
 
-    //function to remove animation once screen has loaded
-    function removeLoader() {
-        $(".overlay-loader").fadeOut(500, function () {
-            // fadeOut complete. Remove the loading div
-            $("#overlay-loader").remove();
-            $('.movieList').css("display", "inline-block")
-        });
-    }
-
-
-//     //loop through the array and check if the move title: === to #input-movie then delete that ID
-//     let id = movies.id
-//     console.log(id)
-
-    function movieDelete() {
-
-                let movieToDelete = $('#input-movie').val();
-                $('#delete-movie').click(function () {
-                    getMovies().then((movies) => {
-            movies.forEach(({title, rating, id}) => { //loops through each movie in the array
-                let i = id ;
-                console.log(movies);
-                    if (title === $('#input-movie').val()) {
-                        // console.log(movies.title);
-                        console.log(title);
-                        deleteMovie(id);
-                        console.log(id)
-                    } else {
-                        console.log("not a movie")
-                    }
-                });
-
-                    $('#input-movie').val('');
-
-            }).then(movieUpdateDelete) // after item is deleted this will update the hmtl with current movies
-
-        });
-    }
-    movieDelete();
-
-
-    // $("#delete-movie").click(function () {
-    //     $('#insertMovie').html(null);
-    //     movieUpdate2();
-    //
-    // });
-
-
-
-
-
-//     deleteMovie(1).then(data => getMovies().then((movies) => {
-//   console.log('Here are all the books:');
-//   movies.forEach(({title, rating, id}) => {
-//     console.log(`id# ${id} - ${title} - ${rating}.`);
-//   });
-// }).catch((error) => {
-//   alert('Not a movie')
-//   console.log(error);
-// }));
-
-
-    // movies.forEach(movie => movie.title === moviee ? deleteMovie(movie).then(movieUpdate) : console.log("not a movie"));
 });
 
 
