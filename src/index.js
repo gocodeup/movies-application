0f9b4d22b03e0f81537e74ff9c09ba07ab77ee85
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-18 10:26:28 -0500

    Worked on styling and added a jquery plugin for dynamic loading of mavies, needs work

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
     movies.forEach(({title, rating, id, posterUrl, director, actors, plot}) => { //iterate through each movie object and get the title, rating, and id
 
       $('#all-movies').append(`
-    <div class="card">
+    <div class="card item">
         <img class="card-img" src="${posterUrl}">
         <div class="card-body">
           <div class="card-rating"> Rating: ${rating}</div>
@@ -157,3 +157,67 @@ function generateBorderRadiusValue() {
 function getRandomValue() {
   return Math.floor(Math.random() * 50) + 50;
 }
+
+//
+// // --------------- PAGINATION ----------------
+// import InfiniteAjaxScroll from '@webcreate/infinite-ajax-scroll';
+//
+// let nextHandler = function(pageIndex) {
+//   let frag = document.createDocumentFragment();
+//   let limit = 3;
+//   let offset = pageIndex * limit;
+//
+//   for (let i = offset, len = offset + limit; i < len; i++) { // add more cards to the container when the offset is complete
+//     let card = frag.appendChild(document.createElement('div'));
+//
+//     // card.data-id = '${id}';
+//     card.classList.add('item');
+//     card.innerHTML = i + 1;
+//   }
+//
+//   return this.append(Array.from(frag.childNodes))
+//       // indicate that there is a next page to load
+//       .then(() => true);
+// };
+//
+// // hides an element but also takes transition delay into account
+// function hideWithTransition(element) {
+//   return new Promise(function(resolve) {
+//     element.addEventListener('transitionend', resolve, {once: true});
+//
+//     element.style.opacity = '0';
+//   });
+// }
+//
+// window.ias = new InfiniteAjaxScroll('.container', {
+//   item: '.item',
+//   next: nextHandler,
+//   trigger: {
+//     // element: '.trigger',
+//
+//     // alternatively we could pass an Element directly
+//     // element: document.getElementById('btn1'),
+//
+//     // alternatively we could pass a factory function
+//     element: function() {
+//       let el = document.createElement('button');
+//       el.innerText = 'Load More...';
+//       document.querySelector('.status').appendChild(el);
+//
+//       // we have to return the element so IAS can add the necessary event listeners
+//       return el;
+//     },
+//
+//     // first page (pageIndex is -1) is generated, after that we show a load more button
+//     when: (pageIndex) => pageIndex >= 0,
+//
+//     // alternative: only show load more button for specific pages
+//     // when: [2, 4, 6],
+//
+//     // alternative: only show on even pages
+//     // when: (pageIndex) => pageIndex % 2 === 0,
+//
+//     // alternative: only show on uneven pages
+//     // when: (pageIndex) => pageIndex % 2 !== 0,
+//   }
+// });
