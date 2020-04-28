be7b8bc67a5168f6ebccafca275024884605a27d
Student Name <rexsutton2@gmail.cogit>
2020-04-28 12:26:56 -0500

    finished loading element

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
+        // .then(function(data){
+        //
+        // })
       .then(response => response.json());
   }
 };
--- src/index.js
+++ src/index.js
@@ -13,8 +13,31 @@ getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
+    // let html = '<ul>';
+    // for(let user of users){
+    // }	          }
+    // html += '</ul>';
+    // document.body.innerHTML = html;
+    $(document).ready(function(){
+      console.log('doc is ready');
+      $("#loading").hide();
+    let html = '<table>';
+    for(const movie of movies){
+      html += `<thead><tr><th colspan="3"> Movie info</th></tr></thead>` +
+          `<tbody><tr>
+            <td>NAME</td>
+            <td>ID</td>
+            <td>RATING</td>
+            </tr></tbody>`;
+
+    }
+    html += `</table>`;
+    $('body').append(html);
+
+
+
+  })});
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
