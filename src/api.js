4bac6df81875dfb8b542788eacbcab4d8dcc0a2a
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-04-28 16:01:48 -0500

    working on ajax request and pushing them to website.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,23 @@
-module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
-      .then(response => response.json());
+
+
+const movie = {
+  movieListing: () => {
+    fetch('/api/movies', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+        .then( response => response.json() )
+        .then( data => {
+          let html = '';
+            data.forEach((movies) => {
+              console.log(movies);
+              html += document.getElementById('movie-list').innerHTML = movies.title;
+            });
+        })
+        .catch( error => console.error(error));
   }
 };
+
+module.exports = movie;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
-const $ = require ('jQuery');
 
+const movie = require('./api');
 
-// const {getMovies} = require('./api.js');
+movie.movieListing();
 
-//
-// getMovies().then((movies) => {
-//   let movieListing = '';
-//   console.log('Here are all the movies:');
-//   movies.forEach(({title, rating, id}) => {
-//
-//     console.log(`id#${id} - ${title} - rating: ${rating}`);
-//
-//       movieListing += '<ul>';
-//
-//       movieListing += `<li>id#${id} - ${title} - rating: ${rating}</li>`
-//
-//       movieListing += `</ul>`;
-//
-//   })
-//   $('#movie-list').html(movieListing);
-// }).catch((error) => {
-//   alert('Oh no! Something went wrong.\nCheck the console for details.');
-//   console.log(error);
-// });
-
-const blogPost = {title: 'Ajax Requests', body: 'Are a fun way to use JS!'};
-const url = '/posts';
-const options = {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(blogPost),
-};
-fetch(url, options)
-    .then(/* post was created successfully */)
-    .catch(/* handle errors */);
