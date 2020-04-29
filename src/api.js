32ec441b29f52c7a84ebd5f7fd3e81df28b88bbf
SUNG IL LEE <sungil.lee90@gmail.com>
2020-04-29 10:56:07 -0500

    feat: append to HTML.

--- src/api.js
+++ src/api.js
@@ -5,20 +5,3 @@ module.exports = {
   }
 };
 
-//  http://localhost:1313/
-// const API = {
-//   createLists: () => {
-//     fetch("/api/movies", {
-//       method: 'GET',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     })
-//         .then( response => response.json() )
-//         .then( data => console.log(data) )
-//         // .then($('body').html(`id#${id} - ${title} - rating: ${rating}`))
-//         .catch( error => console.error(error));
-//   }
-// };
-//
-// module.exports = API;
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const $ = require('jquery');
 import sayHello from './hello';
 sayHello('World');
 
-$('#load_message').html('loading...');
+// $('#load_message').html('loading...');
 
 console.log('movie time!');
 
@@ -20,23 +20,34 @@ console.log('movie time!');
 
 const {getMovies} = require('./api.js');
 
-// getMovies().then(data => data)
-
-// const API = require('./api');
-
-// API.createLists();
-// console.log(API.createLists());
-
-
-
+let htmlBody = "<table style= \"width:80%\">";
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
+  $("#main").empty();
+
   movies.forEach(({title, rating, id}) => {
 
     console.log(`id#${id} - ${title} - rating: ${rating}`);
-  // $('#load_message').html(`id#${id} - ${title} - rating: ${rating}`)
-  });
+
+    htmlBody += `<tr>
+                    <th>Title</th>
+                    <th>Rating</th>
+                    <th>id</th>
+                </tr>
+                <tr>
+                    <td>${title}</td>
+                    <td>${rating}</td>
+                    <td>${id}</td>
+                </tr>
+                </table>`
+
+
+  };
+
+  $('#main').html(htmlBody);
+
+  );
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
