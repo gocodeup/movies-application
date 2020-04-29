b9ba7e6facced5e66e09d8def6d0b27cc68c68a3
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-04-29 09:51:15 -0500

    working on HTML change

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import sayHello from './hello';
 sayHello('World');
 
 $('#load_message').html('loading...');
+
 console.log('movie time!');
 
 
@@ -17,19 +18,24 @@ console.log('movie time!');
  * require style imports
  */
 
-
-const API = require('./api');
-
-API.createLists();
-
-
 const {getMovies} = require('./api.js');
+
+// getMovies().then(data => data)
+
+// const API = require('./api');
+
+// API.createLists();
+// console.log(API.createLists());
+
+
+
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
 
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+  // $('#load_message').html(`id#${id} - ${title} - rating: ${rating}`)
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
