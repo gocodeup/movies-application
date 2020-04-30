bec7400faaf9f123466ac66c9ddb5eb46dc2cbdd
DeLayne LaBove <delayne.labove@gmail.com>
2020-04-29 21:43:20 -0500

    Added the edit movie function

--- src/api.js
+++ src/api.js
@@ -24,6 +24,32 @@ const movie = {
             .catch(/* handle errors */);
     },
 
+
+
+    // EDIT MOVIE
+    editMovie: (j) => {
+        j.preventDefault();
+        let editTitle = $('#title-edit').val();
+        let editRating = $('#edit-movie-rating').val();
+        const editData = {title: editTitle, rating: editRating};
+        const url = '/api/movies';
+        const addMovieEdit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(editData),
+        };
+
+        return fetch(url, addMovieEdit)
+            .then(() => {
+            })
+            .catch(/* handle errors */);
+    },
+
+
+
+
     //Get movie
 
 
@@ -40,14 +66,14 @@ const movie = {
             let movieTitle = '';
             let html = '';
             data.forEach((movies) => {
-            html += (`Title: ${movies.title} Rating: ${movies.rating} Id: ${movies.id}<button data-id=${movies.id} class="movie_edit" style="border: 1px solid">edit</button><br>`);
+            html += (`Title: ${movies.title} Rating: ${movies.rating} Id: ${movies.id}<button data-id=${movies.id} class="movie_edit" >edit</button><br>`);
             movieTitle = `${movies.title}`
             });
             $('#movie-list').html(html);
             $('.movie_edit').click(function() {
-                // let movie_id = $(this).data('id');
-                // console.log(movie_id);
-                $('#title-edit').val(movieTitle);
+                let movie_id = $(this).attr("data-id");
+                console.log(movie_id);
+                $('#movie-id').val(movie_id);
             });
         })
         .catch( error => console.error(error));
--- src/index.js
+++ src/index.js
@@ -7,3 +7,6 @@ movie.movieListing();
 $('#add-movie-btn').click((e) => {
     movie.addMovie(e).then(()=>{movie.movieListing()})
 });
+$('#edit-movie-btn').click((j) => {
+    movie.editMovie(j).then(()=>{movie.movieListing(e)})
+});
