5ad23e80e9d6fbbc3dd28fbf21332cda54b791e1
Justin McCaleb <justinleemccaleb@gmail.com>
2020-05-28 15:28:58 -0500

    working on edit movie funtionality

--- src/index.js
+++ src/index.js
@@ -11,83 +11,90 @@ sayHello('World');
  */
 const {getMovies} = require('./api.js');
 
-function updateMovieList() {
-  getMovies().then((movies) => {
-    $('.container').html('');
-    console.log(movies);
-    movies.forEach(({title, rating, id}) => {
-      // console.log(`id#${id} - ${title} - rating: ${rating}`);
-      $('.container').append(`<div>id#${id} - ${title} - rating: ${rating}</div>`);
+
+//=======================================================
+//adds movies from json movie list to html
+  function updateMovieList() {
+    getMovies().then((movies) => {
+      $('.container').html('');
+      console.log(movies);
+      movies.forEach(({title, rating, id}) => {
+        // console.log(`id#${id} - ${title} - rating: ${rating}`);
+        $('.container').append(`<div>id#${id} - ${title} - rating: ${rating}</div>`);
+      });
+    }).catch((error) => {
+      alert('Oh no! Something went wrong.\nCheck the console for details.');
+      console.log(error);
     });
-  }).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.');
-    console.log(error);
+  }
+  updateMovieList();
+
+//=======================================================
+//Click event that updates json movie list with user input
+  $('#mbutton').click((e) => {
+    e.preventDefault();
+
+    // store inputs for new movie
+    const newTitle = $('#mtitle').val();
+    const newRating = $('#mrating').val();
+
+  console.log(`${newTitle}`);
+  console.log(`${newRating}`);
+
+    const moviePostTest = {title: `${newTitle}`, rating: `${newRating}`};
+    const url = '/api/movies';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(moviePostTest),
+    };
+    fetch(url, options)
+        .then(response => {
+          return response;
+        })
+        .catch(response => console.log('Failed'));
+
+
+    updateMovieList()
+
   });
-}
-updateMovieList();
+
+//=======================================================
+  function modifyMovieList() {
+
+    getMovies()
+
+        .then((movies) => {
+            movies.forEach(({title, rating, id}) => {
+              $('#movieDropDown').append(`<option value="${id}">${title}</option>`);
+            });
+        })
 
 
+        .catch((error) => {
+            alert('Oh no! Something went wrong.\nCheck the console for details.');
+            console.log(error);
 
-// create new movie object for json
-// const newMovie = {title: `${newTitle}`, rating: `${newRating}`, id: ''};
-
-// console.log(newMovie);
-
-// POST to DB
-// const moviePostTest = {title: 'Toy Story', rating: "5", id: 4};
-// const url = '/api/movies';
-// const options = {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(moviePostTest),
-// };
-// fetch(url, options)
-//     .then(response => {
-//       console.log(response);
-//     })
-//     .catch(response => console.log('Failed'));
+        });
 
 
-$('#mbutton').click((e) => {
-  e.preventDefault();
-
-  // store inputs for new movie
-  const newTitle = $('#mtitle').val();
-  const newRating = $('#mrating').val();
-
-console.log(`${newTitle}`);
-console.log(`${newRating}`);
-
-  const moviePostTest = {title: `${newTitle}`, rating: `${newRating}`};
-  const url = '/api/movies';
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(moviePostTest),
-  };
-  fetch(url, options)
-      .then(response => {
-        return response;
-      })
-      .catch(response => console.log('Failed'));
+  }
+modifyMovieList();
 
 
-  // $('.container').html('');
-  // getMovies().then((movies) => {
-  //   console.log(movies);
-  //   movies.forEach(({title, rating, id}) => {
-  //     // console.log(`id#${id} - ${title} - rating: ${rating}`);
-  //     // $('#loading-message').html('');
-  //     $('.container').append(`<div>id#${id} - ${title} - rating: ${rating}</div>`);
-  //   });
-  // }).catch((error) => {
-  //   alert('Oh no! Something went wrong.\nCheck the console for details.');
-  //   console.log(error);
-  // });
-  updateMovieList()
+  $('#dropDownButton').click((e) => {
+    e.preventDefault();
 
-});
+    getMovies()
+
+        .then((movies) => {
+          console.log(movies[0].title);
+          $('#editMovieForm').append(
+              `<input type="text" id="mtitleEdit" name="mtitleEdit" value="${movies.title}">`
+          )
+
+        })
+
+  });
