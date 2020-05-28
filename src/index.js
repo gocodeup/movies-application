e6616643070b3baa56d70b4aa998591aba972d36
Chris Eckert <chris.eckert08@gmail.com>
2020-05-28 13:13:11 -0500

    started parsing to json db

--- src/index.js
+++ src/index.js
@@ -22,3 +22,51 @@ getMovies().then((movies) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
+// store inputs for new movie
+const newTitle = $('#mtitle').val();
+const newRating = $('#mrating').val();
+
+// stores current length of movie list
+const moviesList = getMovies().then((movies) => console.log(movies.length));
+
+// create new movie object for json
+const newMovie = {title: `${newTitle}`, rating: `${newRating}`, id: `${moviesList}`};
+
+// console.log(`${newTitle}`);
+// console.log(`${newRating}`);
+console.log(newMovie);
+
+// POST to DB
+// const moviePostTest = {title: 'Toy Story', rating: "5", id: 4};
+// const url = '/api/movies';
+// const options = {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   body: JSON.stringify(moviePostTest),
+// };
+// fetch(url, options)
+//     .then(response => {
+//       console.log(response);
+//     })
+//     .catch(response => console.log('Failed'));
+
+
+$('#mbutton').click(() => {
+  const moviePostTest = {title: `${newTitle}`, rating: `${newRating}`, id: `${moviesList().length + 1}`};
+  const url = '/api/movies';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(moviePostTest),
+  };
+  fetch(url, options)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(response => console.log('Failed'));
+});
